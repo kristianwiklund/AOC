0c1e98297dccd9ffc6c488789c6690ac09c45f14
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-12 19:26:04 +0100

    banana!

--- 12/tt.erl
+++ 12/tt.erl
@@ -1,4 +1,4 @@
--module(t).
+-module(tt).
 -export([gravity/2]).
 -export([vadd/2]).
 -export([vsub/2]).
@@ -11,7 +11,15 @@
 -export([movemoons/2]).
 -export([energy/1]).
 -export([stupidmove/3]).
--export([run/0]).
+-export([stripmoonsx/2]).
+-export([stripmoonsy/2]).
+-export([stripmoonsz/2]).
+-export([testmoons2/0]).
+-export([flopp/0]).
+-export([lmax/1]).
+-export([x/0,y/0,z/0]).
+-export([x2/0,y2/0,z2/0]).
+-export([gcd/2,lcm/2]).
 
 s(A,B) ->
     %io:fwrite("~w,~w\n",[B,A]),
@@ -21,24 +29,27 @@ s(A,B) ->
 	    0
     end.
 
-gravity({X1,Y1,Z1},{X2,Y2,Z2}) ->
-    
+gravity({X1},{X2}) ->    
     DX = s(X1,X2),
-   DY = s(Y1,Y2),
-    DZ = s(Z1,Z2),
-    {DX,DY,DZ}.
+    {DX}.
 
-vadd({X1,Y1,Z1},{DX,DY,DZ}) ->
-    {X1+DX,Y1+DY,Z1+DZ}.
+vadd({X1},{DX}) ->
+    {X1+DX}.
 
-vsub({X1,Y1,Z1},{DX,DY,DZ}) ->
-    {X1-DX,Y1-DY,Z1-DZ}.
+vsub({X1},{DX}) ->
+    {X1-DX}.
 
-testmoons() ->
+testmoons2() ->
     [{0, {-1,0,2},{0,0,0}},
      {1, {2,-10,-7},{0,0,0}},
      {2, {4,-8,8},{0,0,0}},
      {3, {3,5,-1},{0,0,0}}].
+
+testmoons() ->
+    [{0, {-8,-10,0},{0,0,0}},
+     {1, {5,5,10},{0,0,0}},
+     {2, {2,7,3},{0,0,0}},
+     {3, {9,-8,-3},{0,0,0}}].
 
 
 moons() ->
@@ -65,7 +76,7 @@ newspeed(_,_,Acc) ->
     Acc.
 
 runall(Moons, [W|WS], Acc) ->
-    runall(Moons, WS, Acc++[newspeed(W, Moons,{0,0,0})]);
+    runall(Moons, WS, Acc++[newspeed(W, Moons,{0})]);
 runall(_,_,Acc) ->
     Acc.
 
@@ -101,8 +112,8 @@ energy(Moons) ->
 
 % --
 
-comparepos1({_, A,_},{_,B,_}) ->
-    A==B.
+comparepos1({_,A,C},{_,B,D}) ->
+    (A==B).
 
 comparepos(true, [N|New], [O|Orig]) ->
     comparepos(comparepos1(N,O), New, Orig);
@@ -110,6 +121,43 @@ comparepos(false, _, _) ->
     false;
 comparepos(true, _, _) ->
     true.
+
+stripmoonsx([M|Moons], Acc) ->
+    {ID, {X,_,_}, {DX,_,_}} = M,
+    stripmoonsx(Moons, Acc ++ [{ID, {X}, {DX}}]);
+stripmoonsx(_,Acc) ->
+    Acc.
+
+stripmoonsy([M|Moons], Acc) ->
+    {ID, {_,Y,_}, {_,DY,_}} = M,
+    stripmoonsy(Moons, Acc ++ [{ID, {Y}, {DY}}]);
+stripmoonsy(_,Acc) ->
+    Acc.
+
+stripmoonsz([M|Moons], Acc) ->
+    {ID, {_,_,Z}, {_,_,DZ}} = M,
+    stripmoonsz(Moons, Acc ++ [{ID, {Z}, {DZ}}]);
+stripmoonsz(_,Acc) ->
+    Acc.
+
+
+x2() ->
+    stupidmove(stripmoonsx(moons(),[]), stripmoonsx(moons(),[]),0).
+
+y2() ->
+    stupidmove(stripmoonsy(moons(),[]), stripmoonsy(moons(),[]),0).
+
+z2() ->
+    stupidmove(stripmoonsz(moons(),[]), stripmoonsz(moons(),[]),0).
+
+x() ->
+    stupidmove(stripmoonsx(testmoons(),[]), stripmoonsx(testmoons(),[]),0).
+
+y() ->
+    stupidmove(stripmoonsy(testmoons(),[]), stripmoonsy(testmoons(),[]),0).
+
+z() ->
+    stupidmove(stripmoonsz(testmoons(),[]), stripmoonsz(testmoons(),[]),0).
 
 
 stupidmove(Moons, OrigMoons, Step) ->
@@ -123,18 +171,30 @@ stupidmove(Moons, OrigMoons, Step) ->
 	    stupidmove(NewMoons, OrigMoons, Step+1)
     end.
 
-run() ->
-    T = stupidmove(moons(), moons(), 0),
-    io:fwrite("~B\n", [T]).
 
     
     
+lmax(XS)->
+    lists:foldl(fun(X,Acc) ->
+			if (X>Acc) ->
+				X;
+			   true -> Acc
+			end
+			end, 0, XS).
 
-    
-    
-	    
-    
+gcd(A, 0) ->
+    A;
+gcd(A,B) ->
+    gcd (B, A rem B).
 
+lcm(A,B) ->    
+   trunc(abs(A*B)/gcd(A,B)).
+
+lcm([X]) ->
+    X.
+
+flopp() ->
+    lcm(x2(),lcm(y2(),z2())).
 
 
     
