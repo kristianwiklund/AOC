9e789376821fa76eae1a8fd193874aecfabb7ed2
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-16 19:00:03 +0100

    x

--- 16/t.erl
+++ 16/t.erl
@@ -1,6 +1,6 @@
 -module(t).
 -export([t/0,t2/0, pattern/2,applypattern/1,pmatrix/1,applypattern/2,datan/0]).
--export([app2/3,varannan/3]).
+-export([app2/3,varannan/3, interleave/4]).
 
 datan() ->
     "59790677903322930697358770979456996712973859451709720515074487141246507419590039598329735611909754526681279087091321241889537569965210074382210124927546962637736867742660227796566466871680580005288100192670887174084077574258206307557682549836795598410624042549261801689113559881008629752048213862796556156681802163843211546443228186862314896620419832148583664829023116082772951046466358463667825025457939806789469683866009241229487708732435909544650428069263180522263909211986231581228330456441451927777125388590197170653962842083186914721611560451459928418815254443773460832555717155899456905676980728095392900218760297612453568324542692109397431554".
@@ -60,25 +60,34 @@ varannan([S1|Signal], B1, B2) ->
 varannan(_,_,_) ->
     0.
 
+interleave(List, Cnt, Offs, Sign) ->
+    if Cnt > length(List) ->
+	    0;
+       true ->
+	    Sign*lists:nth(Cnt, List)+
+		interleave(List, Cnt+Offs, Offs, -Sign)
+    end.
+
 app2(Signal, Row, Acc) ->
     Length = length(Signal),
-    
     if 
 	Row>Length ->
 	    {[],0};
 	Row>trunc(Length)/2 -> 
 	    {AccP, Sum} = app2(Signal, Row+1, Acc),
 	    SumP = lists:nth(Row, Signal)+Sum,
-	    {[abs(SumP rem 10)|AccP], SumP};
-	% this covers half the matrix
+	    {[(SumP)|AccP], SumP};
+
+	Row == 1 ->
+	    Sum=varannan(Signal,1,0),
+	    {AccP, SumP} = app2(Signal, Row+1, Acc),
+	    {[(Sum)|AccP], Sum+SumP};
+
 	Row =< trunc(Length)/2 ->
 	    {AccP,Sum} = app2(Signal, Row+1, Acc), % everything up to this line
-	    SumP = lists:nth(Row, Signal) + Sum - lists:nth(Row,AccP),
-	    {[abs(SumP rem 10)|AccP], SumP};	    
+	    SumP = lists:nth(Row, Signal) + Sum - interleave(AccP,Row,2,1),
+	    {[(SumP)|AccP], SumP};	    
 % row 1 is special
-	Row == 1 ->
-	    SumP=varannan(Signal,1,0),
-	    {[abs(SumP rem 10)|Acc], SumP};
 	true ->
 	    {[],0}
     end.
