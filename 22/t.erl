6b8ec9dda1becfc66b5ef828b4b9d4a37d87aa4e
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-22 20:30:22 +0100

    nopes..

--- 22/t.erl
+++ 22/t.erl
@@ -1,6 +1,6 @@
 -module(t).
 -export([t/0,t1/0,dealinto/1, cut/2, dealwithincrement/2]).
--export([rdwi/3,dwi/3]).
+-export([rdwi/3,dwi/3, gcd/2, rcut/3, rdealinto/2,bignum/0]).
 dealinto(Deck) ->
 	lists:reverse(Deck).
 
@@ -9,21 +9,16 @@ cut(Deck, N) when N>0 ->
     Post++Pre;
 cut(Deck, N) when N<0 ->
     U = length(Deck)+N,
-    io:fwrite("~B,~B\n", [N,U]),
+%    io:fwrite("~B,~B\n", [N,U]),
     cut(Deck, U);
     
 cut(Deck, N) when N==0 ->
     Deck.
 
 dealwithincrement([Card|Deck], Acc, Pos, Max, N) ->
-    AccP = maps:put(Pos, Card, Acc),
-    PosP = Pos + N,
-    if PosP > Max ->
-	    PosY = PosP - Max;
-       true  ->
-	    PosY = PosP
-    end,
-    dealwithincrement(Deck, AccP, PosY, Max, N);
+    AccP = maps:put(dwi(Pos,Max,N), Card, Acc),
+
+    dealwithincrement(Deck, AccP, Pos+1, Max, N);
 
 dealwithincrement([],Acc,_,_,_) ->
     Acc.
@@ -43,7 +38,7 @@ t1() ->
     {NT,_} = lists:mapfoldl(
 	   fun(X,Acc)->if X==What->{Acc,Acc+1};true->{0,Acc+1} end end,0, O),
     {value,N} = lists:search(fun (X)->X=/=0 end, NT),
-    {N,O}.
+    N.
 
 % part 2, factory deck of 119315717514047 cards with the input repeated 101741582076661 times...
 % what is the number of the card in position 2020
@@ -69,18 +64,54 @@ t1() ->
 % position 7 -> orig 8
 % position 11 -> orig 15
 
+% this is a prime
+bignum() ->
+    119315717514047.
+
 % %[0,1, 2,   3,4, 5,   6,7, 8,   9,10,11,   12,13,14,  15,16],3).
 dwi(OrigPos, NrCards, Incr) ->
     (OrigPos*Incr) rem NrCards.
 
+gcd(A, 0) -> A;
+gcd(A, B) ->
+    gcd(B, A rem B).
+
+
+%modinv(A, B) ->
+%    {G,X,_} = xgcd(A,B),
+%    if G == 1 ->
+%	    X rem B;
+%       true ->
+%	    throw(error)
+%    end.
+
+   
+  
+% this is broken.
 rdwi(CurrentPos, NrCards, Incr) ->
+    CurrentPos / Incr.
     
-    % the original position is translated through
-    % currentpos = (origpos)*incr rem NrCards
-    % this is what we need to reverse
-    WhereInGroup = CurrentPos rem Incr,
-    WhichGroup = CurrentPos div Incr,
-    
+% backwards cut, where did CurrentPos come from?
+rcut(CurrentPos, CutFactor, Max) when CutFactor < 0 ->
+    CPP = CurrentPos + CutFactor,
+    if CPP < 0 ->
+	    CPP+Max;
+       true ->
+	    CPP
+    end;
+rcut(CurrentPos, CutFactor, _) when CutFactor == 0 ->
+    CurrentPos;
+rcut(CurrentPos, CutFactor, Max) when CutFactor > 0 ->
+    CPP = CurrentPos + CutFactor,
+    if CPP > Max ->
+	    CPP - Max;
+       true ->
+	    CPP
+    end.
+
+rdealinto(CurrentPos, Max) ->
+    % reverse
+    Max-CurrentPos.
     
 
 t()->    
---
+++ 22/test.py
@@ -0,0 +1,14 @@
+def xgcd(a, b):
+    """return (g, x, y) such that a*x + b*y = g = gcd(a, b)"""
+    x0, x1, y0, y1 = 0, 1, 1, 0
+    while a != 0:
+        q, b, a = b // a, a, b % a
+        y0, y1 = y1, y0 - q * y1
+        x0, x1 = x1, x0 - q * x1
+    return b, x0, y0
+
+def modinv(a, b):
+    """return x such that (x * a) % b == 1"""
+    g, x, _ = xgcd(a, b)
+    if g == 1:
+        return x % b
