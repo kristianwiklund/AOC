b7dbe13f2005b371acf8854422f64cb134eedb60
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-27 17:51:04 +0100

    7b works with test case

--- 2018/7/7.py
+++ 2018/7/7.py
@@ -4,39 +4,59 @@ import matplotlib.pyplot as plt
 G = nx.DiGraph()
 
 with open ("shortinput.txt") as fd:
+#with open ("input.txt") as fd:
 
     for  line in fd:
         x = line.split(" ")
         before = x[1]
         after = x[7]
-        G.add_edge(before, after)
+        G.add_edge(before, after, weight=ord(after)-64)
 
 nx.draw(G,  with_labels=True)
 plt.savefig("maze.png")
-print("7A :"+"".join(list(nx.lexicographical_topological_sort(G))))
+helalistan=list(nx.lexicographical_topological_sort(G))
+print("7A :"+"".join(helalistan))
 
 # ---------------------
 
 #ACHOQRXSEKUGMYIWDZLNBFTJVP
 
-def canwe (G, node, done):
+time=0
+workers = [0,0]
+doing = [None, None]
 
-    # try pulling the current node
-    # to be able to do that, all predecessors to the node must be in the "done" set
+while list(G.nodes()) != []:
 
-    pre = G.predecessors(node)
+    for i in range(0,2):
 
-    if set(pre) > set(done):
-        print("cannot pull "+current+" due to preconditions")
-        return False
+        if workers[i] <= 0:
+            # finish what was done, then pull something
+            if doing[i]:
+                print ("Worker "+str(i)+" is done with "+doing[i])
+                G.remove_node(doing[i])
+                doing[i] = None
+                
+            for j in helalistan:
+                #print ("Trying to pull node "+j)
+                if not j in doing:
+                    #print ("Nobody is working on "+j)
+                    if G.has_node(j) and list(G.predecessors(j)) == []:
+                        print ("Worker "+str(i)+" pulls node "+j)
+                        doing[i] = j
+                        workers[i] = ord(j)-65
+                        break
+            
+        else:
+            workers[i] = workers[i] - 1
 
-    # do the work
-
-    return True
-
-
-def work(G):
-
-    for i in G:
+    print("Tick: "+str(time) + " working on "+str(doing))
+    time=time+1
 
 
+print(time-1)
+
+    
+    
+    
+    
+    
