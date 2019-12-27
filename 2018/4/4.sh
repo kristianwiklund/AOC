9522e5d74244a48bdf1cdd60ab93bf36d671ef7c
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-27 02:00:54 +0100

    2018 4b

--- 2018/4/4.sh
+++ 2018/4/4.sh
@@ -25,11 +25,23 @@ awk -f 4.awk < processed.tmp | tr -d "#" > combined.tmp
 # strategy 1: which guard have the most sleepy time
 worstguard=`awk '{guard[$1]=guard[$1]+$2} END {for (i in guard) {print guard[i],i}}' < combined.tmp | sort -n  | tail -1| cut -d ' ' -f2`
 
-echo -n "strategy 1: the worst guard is $worstguard, sleeping "
+echo -n "Strategy 1: The worst guard is $worstguard, sleeping "
 
 (grep "^$worstguard" combined.tmp| cut -d ' ' -f4,7 | sed 's/^/seq /' | bash) | sort | uniq -c | sort -n | tail -1 | tr -s " " | sed -e 's/^ //' -e 's/ / times on minute /'
 
 # ----
+
+# strategy 2:
+
+rm -f minutes.tmp
+
+for i in `cut -d ' ' -f1 combined.tmp  | sort -u`; do
+
+    (grep "^$i " combined.tmp| cut -d ' ' -f4,7 | sed 's/^/seq /' | bash) | sed "s/^/$i /" >> minutes.tmp
+    
+done
+
+ cat minutes.tmp  | sort | uniq -c | sort -bn  | tail -1 | awk '{print "Strategy 2: Guard", $2, "slept", $1,"times on minute",$3}'
 
 
 
