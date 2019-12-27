181d87cd5fd70a64c442219d8bb29246396d2516
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-27 02:22:41 +0100

    2018 5a

---
+++ 2018/5/5.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -f sed.tmp
+
+for i in {a..z}; do
+    echo "s/$i${i^^}//g" >> sed.tmp
+    echo "s/${i^^}$i//g" >> sed.tmp
+done
+
+#cp shortinput.txt input.tmp
+cp input.txt input.tmp
+
+i=-1
+j=0
+
+while [ "$i" -ne "$j" ]; do
+    i=$j
+    sed -f sed.tmp < input.tmp > apa.tmp
+    mv apa.tmp input.tmp
+    j=`wc -c input.tmp|cut -d ' ' -f 1`
+    echo $j
+done
+cat input.tmp | tr -d " \n" | wc -c
+
+
