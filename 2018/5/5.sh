c60b1faa4f803fcd691bc7ef729b4ee5ca57e8f1
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-27 02:31:50 +0100

    5b 2018

--- 2018/5/5.sh
+++ 2018/5/5.sh
@@ -7,7 +7,7 @@ for i in {a..z}; do
     echo "s/${i^^}$i//g" >> sed.tmp
 done
 
-#cp shortinput.txt input.tmp
+
 cp input.txt input.tmp
 
 i=-1
@@ -20,6 +20,30 @@ while [ "$i" -ne "$j" ]; do
     j=`wc -c input.tmp|cut -d ' ' -f 1`
     echo $j
 done
+
 cat input.tmp | tr -d " \n" | wc -c
 
+# part 2
 
+rm -f result.tmp
+
+for l in {a..z}; do
+
+    echo "$l"
+    cat input.txt | tr -d " \n$l${l^^}" > input.tmp
+    
+    i=-1
+    j=0
+
+    while [ "$i" -ne "$j" ]; do
+	i=$j
+	sed -f sed.tmp < input.tmp > apa.tmp
+	mv apa.tmp input.tmp
+	j=`wc -c input.tmp|cut -d ' ' -f 1`
+    done
+
+    echo "$j $l" >> result.tmp
+    
+done
+
+sort -rn result.tmp | tail -1
