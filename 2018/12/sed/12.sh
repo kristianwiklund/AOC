515dcac3678a15da53cb2e4250c0b5ffbff58f6f
Kristian Wiklund <githubkristian@snabela.nl>
2019-12-28 02:17:04 +0100

    ix

--- 2018/12/sed/12.sh
+++ 2018/12/sed/12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INPUT=input
+INPUT=2input
 
 echo "h"> sed.tmp
 cat ${INPUT}_actions.txt | sed -e "s/ => /?/" -e "s/$/?g/" -e "s/^/s?/" -e "s/?#?/?\\\1#\\\2?/" -e "s/?-?/?\\\1-\\\2?/" -e "s/s?\(..\)\(.\)\(..\)/s?\\\\(\1\\\\)\2\\\\(\3\\\)/" -e "s/\./-/g" | sed 's/$/\np\ng/' >> sed.tmp
@@ -13,11 +13,14 @@ sed -f sed.tmp $INPUT.txt | sort -u | grep -v -f $INPUT.txt > tmp.tmp
 rm -f sed2.tmp
 
 for i in `cat tmp.tmp`; do
-    echo $i | cmp -l $INPUT.txt - 
     echo $i | cmp -l $INPUT.txt - | awk '{printf("s/^\\(.\\{%d\\}\\)./\\1%c/\n",$1-1,strtonum("0"$3))}' >> sed2.tmp
 
     #| awk '{print "s/^\\(.\\{"$5-1"\\}\\)./\\1"$12"/"}' >>sed2.tmp
 done
 
-cat $INPUT.txt | sed -f sed2.tmp
+cat $INPUT.txt | sed -f sed2.tmp > tmp.in
+mv tmp.in $INPUT.txt
+awk -vFS="" '{for(i=1;i<=NF;i++)w[$i]++}END{for(i in w) print i,w[i]}' < $INPUT.txt
+cat $INPUT.txt
+
 
