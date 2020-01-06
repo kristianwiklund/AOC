306b8189eeb67fd2970c959e07852f520a0478c0
Kristian Wiklund <githubkristian@snabela.nl>
2020-01-06 23:41:58 +0100

    2015 5 2/2

--- 2015/5/5.sh
+++ 2015/5/5.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo "Part 1:"
 cat input.txt | grep "[aeiou].*[aeiou].*[aeiou]" | grep "\(.\)\1" | egrep -v "ab|cd|pq|xy" | wc -l
+
+echo "Part 2:"
+cat input.txt | grep "\(..\).*\1" | grep "\(.\).\1" | wc -l
