98e267bc3126b8c4a88e43f183aed51d7dd51c58
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-02 10:20:05 +0100

    2020/2-1 (bash)

---
+++ 2020/2/1.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# process one line at a time
+# $1 is the number of chars allowed (range)
+# $2 is the char plus a :
+# $3 is the password
+
+function x {
+    IFS=- read -a L <<< $1
+    C=`tr -d ':' <<< $2`
+    seq ${L[0]} ${L[1]}|sed -e 's/^/^/' -e "s/\$/$C/" > tmp
+
+    sed 's/\(.\)/\1 /g' <<< $3 | tr ' ' '\n' | sort | uniq -c | tr -d ' ' | grep $C | grep -f tmp
+}
+
+(cat input | while read i; do 
+    x $i
+done) | wc -l
