dd1c96865a2b9eb2cb39d53bad6f541042f5a4d0
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-02 13:28:10 +0100

    1b, also bash

--- 2020/2/1b.sh
+++ 2020/2/1b.sh
@@ -3,12 +3,20 @@
 function x {
      read  -a U <<< `tr '-' '\n' <<< $1`
      C=`tr -d ':' <<< $2`
+
+     D=`expr ${U[1]} - ${U[0]} - 1`
+
+     if [ "$D" != "0" ]; then
+	 (head -c ${U[0]} < /dev/zero ; echo -n $C; head -c $D < /dev/zero;echo "[^$C]") | tr '\0' '.' | sed 's/./^/' > tmp
+	 (head -c ${U[0]} < /dev/zero ; echo -n "[^$C]"; head -c $D < /dev/zero;echo "$C") | tr '\0' '.' | sed 's/./^/' >> tmp
+     else
+	 (head -c ${U[0]} < /dev/zero ; echo -n $C; echo "[^$C]") | tr '\0' '.' | sed 's/./^/' > tmp
+	 (head -c ${U[0]} < /dev/zero ; echo -n "[^$C]"; echo "$C") | tr '\0' '.' | sed 's/./^/' >> tmp
+     fi
      
-     (head -c ${U[0]} < /dev/zero ; echo -n $C; head -c `expr ${U[1]} - ${U[0]}` < /dev/zero;echo "[^$C]") | tr '\0' '.' | sed 's/.//' > tmp
-     (head -c ${U[0]} < /dev/zero ; echo -n "[^$C]"; head -c `expr ${U[1]} - ${U[0]}` < /dev/zero;echo "$C") | tr '\0' '.' | sed 's/.//' >> tmp
      echo $3 | grep -f tmp
 }
 
-(cat input.short | while read i; do
+(cat input | while read i; do
      x $i
-done) 
+done) | wc -l 
