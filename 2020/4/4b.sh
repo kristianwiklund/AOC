f6fb5f9100b752cc8377b448ba2b67d855687f7a
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-04 16:16:23 +0100

    minor fix

--- 2020/4/4b.sh
+++ 2020/4/4b.sh
@@ -7,7 +7,7 @@ cat input | sed ':a;N;$!ba;s/\([a-zA-Z0-9]\)\n/\1 /g' \
     | egrep "hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in) " \
     | grep -E "hcl:#[0-9a-f]{6} " \
     | egrep "ecl:(amb|blu|brn|gry|grn|hzl|oth) " \
-    | grep -E "pid:[0-9]{9} "
+    | grep -E "pid:[0-9]{9} " | wc -l
 
 
 
