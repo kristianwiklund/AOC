b4a9a6cbcd2387ff3fdaf8478ed6673493ea6ad3
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 23:18:28 +0100

    refactoring

--- 2020/1/ett.erl
+++ 2020/1/ett.erl
@@ -1,47 +1,49 @@
 -module(ett).
--export([a/0]).
--export([b/0]).
+-export([a/0,a1/1]).
+-export([b/0,b1/2]).
 
-a() ->
-    [{_,A,B}|_] =
-    lists:filter(fun({V,_,_})->
-			 V == 2020
-		 end,
-		 lists:flatten(lists:map(fun(X)->
-						 lists:map(
-						   fun(Y) ->
-							   {X+Y,X,Y} 
-						   end,
-						   input:long())
+a1(Input) ->
+    lists:flatten(lists:map(fun(X)->
+				    lists:map(
+				      fun(Y) ->
+					      {X+Y,X,Y} 
+				      end,
+				      Input)
 					 end,
-					 input:long()))),
+					 Input)).
+    
+a(Input) ->
+    {_,{_,A,B},_} =
+	lists:keytake(2020,1,a1(Input)),
     A*B.
 
-b1() ->
+b1(Input,D) ->
     lists:filter(fun({V,_,_})->
-			 V < 2020
+			 V < 2020-D
 		 end,
-		 lists:flatten(lists:map(fun(X)->
-						 lists:map(
-						   fun(Y) ->
-							   {X+Y,X,Y} 
-						   end,
-						   input:short())
-					 end,
-					 input:short()))).
+		 a1(Input)).
 
-b() ->
-    [{_,A,B,C}|_] =                                                                                                
-    lists:filter(fun({V,_,_,_}) ->
-			 V == 2020
-		 end,
-    lists:flatten(lists:map(fun(Z) ->
-		      lists:map(
-			fun({S,X,Y})->
-				{S+Z,X,Y,Z}
-			end,
-			b1())
-	      end,
-			    input:long()))),
+b(Input) ->
+    D = lists:min(Input),
+    If = lists:filter(fun(V)->
+			      V < 2020-2*D
+		      end,
+		      Input),
+    {_,{_,A,B,C},_} =                                                                                                
+    lists:keytake(2020,1,
+		  lists:flatten(lists:map(fun(Z) ->
+						  lists:map(
+						    fun({S,X,Y})->
+							    {S+Z,X,Y,Z}
+						    end,
+						    b1(If,D))
+					  end,
+					  If))),
     A*B*C.
-		      
+
+
+a() ->
+    a(input:long()).
+
+b()->		      
+    b(input:long()).
