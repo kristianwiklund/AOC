365a4de907a707edfc6957dde5abb60b24d892e2
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 08:10:54 +0100

    2020/5/1

---
+++ 2020/5/fem.erl
@@ -0,0 +1,40 @@
+-module(fem).
+-export([one/1]).
+-export([rc/3]).
+-export([calc/1]).
+
+rc([R|RS], Len, Row) ->
+    {L,U} = lists:split(Len div 2,Row),
+
+    case R of 
+	70->
+	    rc(RS,Len div 2,L);
+	66->
+	    rc(RS,Len div 2,U);
+	76->
+	    rc(RS,Len div 2,L);
+	82->
+	    rc(RS,Len div 2,U)
+    end;
+rc(_,_,[R|_]) ->
+    R.
+
+
+
+calc([A,B,C,D,E,F,G,H,I,J]) ->
+    R=rc([A,B,C,D,E,F,G],128,lists:seq(0,127)),
+    S=rc([H,I,J],8,lists:seq(0,7)),
+    {R,S,R*8+S};
+calc(_) ->
+    false.
+
+one([X|XS],Acc) ->
+    one(XS, [calc(X)|Acc]);
+one(_,Acc) ->
+    Acc.
+one(X) ->
+    [{_,_,Id}|_] = lists:reverse(lists:keysort(3,one(X,[]))),
+    Id.
+    
+
+    
