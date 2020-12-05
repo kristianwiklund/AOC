55d7e3f4d008c4c977f8e0fbfaf1dee152e5673b
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 23:24:37 +0100

    optimizations

--- 2020/5/5.sh
+++ 2020/5/5.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-(echo ibase=2;cat input.txt | tr 'FBLR' '0101' | sed 's/\(.......\)\(...\)/\1*8+\2/') | bc | sort -n | tail -1
-(echo ibase=2;cat input.txt | tr 'FBLR' '0101' | sed 's/\(.......\)\(...\)/\1*8+\2/') | bc | sort -n > tmp
+(echo ibase=2;cat input.txt | tr 'FBLR' '0101' ) | bc | sort -n | tail -1
+(echo ibase=2;cat input.txt | tr 'FBLR' '0101' ) | bc | sort -n > tmp
 seq `head -1 tmp` `tail -1 tmp` > tmp2
 grep -v -f tmp tmp2
 
