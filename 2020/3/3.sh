0a89b15a154e2fb03dcae3d7a1abcb9aaa6f131c
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 21:16:54 +0100

    added solution for day 3 2020 in shell script

--- 2020/3/3.sh
+++ 2020/3/3.sh
@@ -7,8 +7,13 @@ function f {
     f=$3
     x=0
     sx=`tail -1 $f|tr -d '\n'|wc -c`
-    
-    k=`sed -n "0~${dy}p"<$f|tr '\n' ' '`
+
+    if [ $dy -gt 1 ]; then
+
+	k=`(head -1 $f;tail -n +1 $f)|sed -n "0~${dy}p"|tr '\n' ' '`
+    else
+	k=`sed -n "0~${dy}p"<$f|tr '\n' ' '`
+    fi
 
     (for i in $k; do
 	sed "s/.\{$x\}\(.\).*\$/\1/" <<<$i | grep "#"
@@ -22,17 +27,15 @@ function f {
 
 
 # answer to problem 3/1
-b=`f 3 1 input.short`
-echo $b
+b=`f 3 1 input`
+echo "A=$b"
 
-a=`f 1 1 input.short`
+a=`f 1 1 input`
 # b done above
-c=`f 5 1 input.short`
-d=`f 7 1 input.short`
-e=`f 1 2 input.short`
+c=`f 5 1 input`
+d=`f 7 1 input`
+e=`f 1 2 input`
 
-echo $a
-echo $b
-echo $c
-echo $d
-echo $e
+echo -n "B="
+echo "$a * $b*$c* $d* $e" | bc
+
--- 2020/README.md
+++ 2020/README.md
@@ -4,7 +4,7 @@ Language per day
 ================
 1) shell script 
 2) shell script
-3) python
+3) python (and also later done with shell script)
 4) shell script
 5) Erlang (and a one-liner in bash after I woke up and realize what the algorithm actually was :-))
 
