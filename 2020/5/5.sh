54c08c6133740372a3678790db54323c5f7c1d2a
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 10:50:06 +0100

    and both exercises for 5 / 2020 done in bash as well

--- 2020/5/5.sh
+++ 2020/5/5.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-(echo ibase=2;cat input.short | tr 'FBLR' '0101' | sed 's/\(.......\)\(...\)/\1*8+\2/') | bc | sort -n | tail -1
+(echo ibase=2;cat input.txt | tr 'FBLR' '0101' | sed 's/\(.......\)\(...\)/\1*8+\2/') | bc | sort -n | tail -1
+(echo ibase=2;cat input.txt | tr 'FBLR' '0101' | sed 's/\(.......\)\(...\)/\1*8+\2/') | bc | sort -n > tmp
+seq `head -1 tmp` `tail -1 tmp` > tmp2
+grep -v -f tmp tmp2
 
