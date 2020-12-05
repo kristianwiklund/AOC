005fe55f9333a986d247eee4f1664ce39400b6ac
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-05 22:50:20 +0100

    2020-1 2 in erlang

--- 2020/1/ett.erl
+++ 2020/1/ett.erl
@@ -1,5 +1,6 @@
 -module(ett).
 -export([a/0]).
+-export([b/0]).
 
 a() ->
     [{_,A,B}|_] =
@@ -15,3 +16,32 @@ a() ->
 					 end,
 					 input:long()))),
     A*B.
+
+b1() ->
+    lists:filter(fun({V,_,_})->
+			 V < 2020
+		 end,
+		 lists:flatten(lists:map(fun(X)->
+						 lists:map(
+						   fun(Y) ->
+							   {X+Y,X,Y} 
+						   end,
+						   input:short())
+					 end,
+					 input:short()))).
+
+b() ->
+    [{_,A,B,C}|_] =                                                                                                
+    lists:filter(fun({V,_,_,_}) ->
+			 V == 2020
+		 end,
+    lists:flatten(lists:map(fun(Z) ->
+		      lists:map(
+			fun({S,X,Y})->
+				{S+Z,X,Y,Z}
+			end,
+			b1())
+	      end,
+			    input:long()))),
+    A*B*C.
+		      
