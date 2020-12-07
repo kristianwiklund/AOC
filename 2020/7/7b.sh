77ce65065b2fa2c7a357b955b2adc0a309b3532a
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-07 16:03:04 +0100

    works except two p to much...

--- 2020/7/7b.sh
+++ 2020/7/7b.sh
@@ -1,13 +1,50 @@
 #!/bin/bash
 
-cat $1 | tr -d '.' |sed 's/contain no other bags/1/' | tr ',' '+' > tmp3
+
 
 function l() {
     
-    cat $1 | grep -v contain | sed 's/bags/bag/' | sed "s/\(.* .* .*\) \(.*\)/s#\1s*#*\2#/"
+    cat $1 | grep -v contain | sed 's/bags/bag/' | grep -v "shiny gold" | sed "s/^\([ a-z]*\) \(.*\)/s#\1s*#*\2#/"
 
     # | cut -d ' ' -f 1-3 | sed 's/bags/bag/' | sed 's#\(.*\)#s/\1/shiny gold bag/#'  > tmp
 }
 
-l tmp3 > pattern
-sed -f pattern < tmp3  | grep -v ^*
+function b() {
+
+    until diff "in" "out" > /dev/null; do
+	echo "bopp"
+	cp in result
+	mv "out" "in"
+	cat in | tr -d '.' |sed 's/contain no other bags/1/' | grep -v "[0-9] shiny gold" | tr ',' '+' > tmp3
+
+	l tmp3 > pattern
+	mv tmp3 tmp4
+	touch tmp3
+	
+	until diff tmp3 tmp4 > /dev/null; do
+	    mv tmp4 tmp3
+	    sed -f pattern < tmp3  | grep -v "^*"  > tmp4
+	done
+	
+	grep -v "[0-9]$" < tmp4 | grep -v ")$"  > out
+	
+#	egrep "[0-9]$" < tmp4  | sed 's/^\(.*\) contains*/\1 (1+/' | sed 's/$/)/' >> out
+	egrep "[0-9])*$" < tmp4  | sed 's/^\(.*\) contains*/\1 (1+/' | sed 's/$/)/' >> out
+
+
+	Y=`grep "^shiny gold" result`
+	X=`grep "^shiny gold" result | sed 's/bags//' | grep -v bag`
+	echo $Y	
+	if [ "a$X" != "a" ]; then
+	    echo $X
+	    exit
+	fi
+    done
+}
+rm -f "in"
+touch "in"
+
+ cp input "out"
+ b
+ grep "shiny gold" result
+
