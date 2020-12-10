c4a521484966ca354a71143037b6c6830b249031
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-10 07:52:14 +0100

    refactor

--- 2020/10/10.sh
+++ 2020/10/10.sh
@@ -1,2 +1,4 @@
- cat input | sort -n | awk 'BEGIN {pre=0} {print $1-pre;pre=$1}'  | sort | uniq -c
-echo add 1 to 3, multiply the numbers
+#/!bin/sh
+
+cat input | sort -n | awk 'BEGIN {pre=0} {print $1-pre;pre=$1}'  | sort | uniq -c | awk '{print $1}' | tr '\n' ' ' | awk '{print $1*($2+1)}'
+
