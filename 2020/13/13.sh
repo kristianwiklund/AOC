66a08b2d0675f317c526a545032180c81163ab94
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-13 11:59:37 +0100

    solution for 2020-13 (a) in bash

--- 2020/13/13.sh
+++ 2020/13/13.sh
@@ -1,1 +1,11 @@
-cat input | tr ',' '\n' | grep -v x | sed 's/^\(.*\)/1000510-(1000510\/\1)*(\1)-\1/' | bc
+#!/bin/bash
+
+read -a A  < <(cat input | tr ',' '\n' | grep -v x | sed 's/^\(.*\)/-(1000510-(1000510\/\1)*(\1)-\1)/' | bc | tr '\n' ' ')
+
+read -a B < <(cat input | tr ',\n' '  '|tr -d x | tr -s ' ')
+
+let c=${#A[@]}-1
+(for i in `seq 1 $c` ; do
+    let G="${A[$i]} * ${B[$i]}"
+    echo "${A[$i]} * ${B[$i]} = $G"
+done) | sort -nr | tail -1 | cut -d ' ' -f5
