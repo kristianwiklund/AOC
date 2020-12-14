b3317ca420e6cbca1baa59bab6392f50404b59d1
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-14 19:30:01 +0100

    part 2 done

--- 2020/14/14.sh
+++ 2020/14/14.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-#cat input  | (echo "from mem import Mem";echo "mem=Mem()";awk '/mask/ {print "mem.setmask(\""$3"\")"} /mem/ {print}';echo "print(sum(mem.values()))") > 14.py
+#cat $1  | (echo "from mem import Mem";echo "mem=Mem()";awk '/mask/ {print "mem.setmask(\""$3"\")"} /mem/ {print}';echo "print(sum(mem.values()))") > 14.py
 #python3 14.py
 
-cat input.2.short  | (echo "from mem import Mem2,sumzor";echo "mem=Mem2()";awk '/mask/ {print "mem.setmask(\""$3"\")"} /mem/ {print}';echo "print(sumzor(mem))") > 14.py
+cat $1  | (echo "from mem import Mem2,sumzor";echo "mem=Mem2()";awk '/mask/ {print "mem.setmask(\""$3"\")"} /mem/ {print}';echo "#print (mem)";echo "print(sumzor(mem))") > 14.py
+#cat $1  | (echo "from mem import Mem2,sumzor";echo "mem=Mem2()";awk '/mask/ {print "mem.setmask(\""$3"\")"} /mem/ {print}';echo "print(sumzor(mem))") > 14.py
 python3 14.py
+
+# 4124741710594 - too low
+# 4124741710594
+
--- 2020/14/mem.py
+++ 2020/14/mem.py
@@ -1,4 +1,4 @@
-import re
+import sys
 
 class Mem(dict):
     ormask=0
@@ -26,54 +26,52 @@ class Mem2(dict):
         #        print(mask)
         b = int(mask.replace("X","1"),2)
         self.thekeepmask =   (~b) + 0b111111111111111111111111111111111111
-        #        print("  "+mask)
-        #        print(bin(b).zfill(36))
-        #        print(bin(self.keepmask).zfill(36))
         self.thesetmask = int(mask.replace("X","0"),2)
         #        self.tsmask=mask
-        self.thewildcardmask = mask.replace("1"," ").replace("0"," ").replace("X",".")
+        self.themask = mask
+        #print(self.thewildcardmask)
 
+    def recurse(self, s):
+
+        if len(s) == 0:
+            return [""]            
+        else:
+            p = self.recurse(s[1:])
+        l = list()
+
+        for i in p:
+            if s[0] == 'X':
+                l.append("0"+i)
+                l.append("1"+i)
+            else:
+                l.append(s[0]+i)
+
+        return (l)
         
-    def __setitem__(self, key,value):
+    def __setitem__(self, key, value):
         # wildcard match what already is in the memory
         # replace (fold) those into a single line
         
-        fkey = key & self.thekeepmask
-        fkey = fkey | self.thesetmask
+        #fkey = key & self.thekeepmask
+        #fkey = fkey | self.thesetmask
 
-        bopp = str(bin(fkey))[2:].zfill(36)
-
+        bopp = str(bin(key))[2:].zfill(36)
+        #print("bopp:" +bopp)
         k=list()
         for i in range(0,36):
-            if self.thewildcardmask[i] != ' ':
-                k.append(self.thewildcardmask[i])
+            if self.themask[i] == 'X' or self.themask[i] == '1':
+                k.append(self.themask[i])
             else:
                 k.append(bopp[i])
-
+        
 
         k= str("".join(k))
-
-        if k[35] == '.':
-            lists = ["1","0"]
-        else:
-            lists = [k[i]]
+        #print(k)
+        lists=self.recurse(k)
         
-        for i in range(34,0,-1):
-            if k[i] == '.':
-                what = ['1','0']
-            else:
-                what = [k[i]]
-
-            lists = [[x+y for x in lists for y in what]]
-            lists = sum(lists,[])
-            
         for i in lists:
-            super(Mem2, self).__setitem__(i[::-1], value)
+            super(Mem2, self).__setitem__(i, value)
 
 def sumzor(mem):
 
-    k =0
-    for i in mem.keys():
-        x = sum(map((lambda x:1 if x=='.' else 0), list(i)))
-        k=k+((2**x)*mem[i])
-    return (k)
+    return (sum(mem.values()))
