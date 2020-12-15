9ca5600bf9b8b16138b07079cd85b5c31518f9e0
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-15 20:12:07 +0100

    refactor

--- 2020/15/input.erl
+++ 2020/15/input.erl
@@ -4,7 +4,18 @@
 -export([short3/0]).
 -export([short4/0]).
 -export([short5/0]).
+-export([short6/0]).
+-export([short7/0]).
 -export([input/0]).
+-export([mkim/1]).
+
+mkim(L) ->
+    A = lists:map(fun(X)->
+			  [X]
+		  end,
+		  lists:seq(1,length(L))),
+    B = maps:from_list(lists:zip(L,A)),
+    B.
 
 
 input() ->
@@ -23,3 +34,10 @@ short4() ->
 
 short5() ->
     [2,3,1].
+
+
+short6() ->
+    [3,2,1].
+
+short7() ->
+    [3,1,2].
