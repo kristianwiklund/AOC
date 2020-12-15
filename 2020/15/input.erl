1c6dfaa6a0cae9a5cd7230036a2d875fdc1d8154
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-15 19:14:46 +0100

    2020 15 a

---
+++ 2020/15/input.erl
@@ -0,0 +1,25 @@
+-module(input).
+-export([short1/0]).
+-export([short2/0]).
+-export([short3/0]).
+-export([short4/0]).
+-export([short5/0]).
+-export([input/0]).
+
+
+input() ->
+    [15,12,0,14,3,1].
+short1() ->
+    [0,3,6].
+
+short2() ->
+    [1,3,2].
+
+short3() ->
+    [2,1,3].
+
+short4() ->
+    [1,2,3].
+
+short5() ->
+    [2,3,1].
