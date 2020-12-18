84a094326b3634592b9808e9217083305dfc22bf
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-18 10:06:09 +0100

    restructuring

--- 2020/18/18-c.sh
+++ 2020/18/18-c.sh
@@ -1,2 +1,3 @@
-cat input | sed 's/^\(.*\)$/echo "\1"|.\/a.out/' > ap
+#!/bin/sh
+cat $1 | sed 's/^\(.*\)$/echo "\1"|.\/a.out/' > ap
 (echo "0";bash -f ap 2>&1 | cut -d. -f1 | grep [0-9] | sed 's/$/ +/';echo p) | dc
