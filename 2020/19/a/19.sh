b4e1cea1e8189844f4e85809aaf1aadca15dda7c
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-19 15:49:31 +0100

    2020 19 A with antlr...

--- 2020/19/a/19.sh
+++ 2020/19/a/19.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 echo "grammar ap;"> ap.g4
-grep [0-9] $1 | sed 's/\([0-9][0-9]*\)/T\1/g' | sed 's/$/;/' >> ap.g4
+echo "start: T0 '\\\n' {print(\"yay!\")};" >> ap.g4
+
+grep [0-9] $1 | sed 's/\([0-9][0-9]*\)/T\1/g' | sed 's/$/;/' | tr '"' "'">> ap.g4
 antlr4 -Dlanguage=Python3 ap.g4
+
+(for i in `grep -v -e "[0-9]" -e "^$" $1`; do
+     echo $i
+    echo $i | python3 hello.py
+ done) | tee tmp3
+
+grep yay tmp3 | wc -l
+
