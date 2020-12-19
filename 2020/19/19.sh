08411c56f4d3a5be55ca89799884c15d366d9ffb
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-19 11:03:53 +0100

    junk

--- 2020/19/19.sh
+++ 2020/19/19.sh
@@ -6,7 +6,7 @@ touch tmp
 
 function f() {
 
-    grep [ab] tmp | tr -d '":'| awk '{s = ""; for (i = 2; i <= NF; i++) s = s $i " ";print "s/ "$1" / ( "s") /g";print "s/ "$1"$/ ( "s") /g"}' > rules
+    grep [ab] tmp | tr -d '":'| awk '{s = ""; for (i = 2; i <= NF; i++) s = s $i " ";print "s/ "$1" / ( "s") /g";print "s/ "$1"$/ ( "s") /g"}' | sed -e 's/( a )/a/g' -e 's/( b )/b/g' > rules
     sed -f rules < tmp > tmp2
 }
 
@@ -27,5 +27,8 @@ echo "done looping"
 #mv tmp2 tmp
 #f
 echo "--"
-cat tmp | grep "^0:" | tr -d ' ' | cut -d: -f2 | awk '{print "^"$0"$"}' > rules2
-egrep -f rules2 $1 | wc -l
+cat tmp | grep "^0:"  | tr -d ' ' | cut -d: -f2 | awk '{print "^"$0"$"}' | sed -e 's/(a)/a/g' -e 's/(b)/b/g' | sed 's/(\([^|]\))/\1/g' > rules2
+cat rules2 |   sed 's/[(]/(?:/g' | sed -e 's/ax/)/' -e 's/xa/(/' > rules3
+cat rules3 | sed 's/bobarob/\\1/' | tr -d "'" > rules2
+grep -P -f rules2 $1  | wc -l
+grep 1 rules2
