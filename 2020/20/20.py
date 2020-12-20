ae3c9022541b283b9ee27f45d09f12cb390138c8
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-20 09:12:17 +0100

    reader, rotations, ...

--- 2020/20/20.py
+++ 2020/20/20.py
@@ -1,5 +1,5 @@
 
-def read(f):
+def readone(f):
     # parse one tile
 
     h = f.readline()
@@ -11,26 +11,72 @@ def read(f):
     for i in range(0,10):
         A.append(f.readline().strip('\n\r').replace("#","1").replace(".","0"))
 
+
     # process a signature for each tile
+
+    ll = "".join([x[9] for x in A])
+    lr = "".join([x[0] for x in A]) 
+    
     top = int(A[0],2)
     bottom = int(A[9],2)
-    left = int("".join([x[9] for x in A]),2)
-    right = int("".join([x[0] for x in A]),2)
+    left = int(ll,2)
+    right = int(lr,2)
 
+    rtop = int(A[0][::-1],2)
+    rbottom = int(A[9][::-1],2)
+    rleft = int(ll[::-1],2)
+    rright = int(lr[::-1],2)
+    
     # and discard final empty line
     f.readline()
     
-    return (myid, top,bottom,left,right)
+    return (myid, top,bottom,left,right,rtop,rbottom,rleft,rright)
 
-with open("input.short","r") as fd:
-    pics = list()
-    try:
-        while True:
-            pics.append(read(fd))
-    except:
-        pass
+def fliphoriz(tile):
 
-    print(pics)
+    (myid, top,bottom,left,right,rtop,rbottom,rleft,rright) = tile
+
+    return (myid, rtop, rbottom, right, left, top, bottom, rright, rleft)
+
+def flipvert(tile):
+
+    (myid, top,bottom,left,right,rtop,rbottom,rleft,rright) = tile
+
+    return (myid, top, bottom, rleft, rright, rtop, rbottom, left, right)
+
+def rot90(tile):
+    (myid, top,bottom,left,right,rtop,rbottom,rleft,rright) = tile
+
+    return (myid, left, right, rtop,rbottom,rleft,rright, top ,bottom)
+
+def rot180(tile):
+
+    return(rot90(rot90(tile)))
+
+def rot270(tile):
+
+    return(rot90(rot180(tile)))
+
+def getpix(fname):
+    with open(fname,"r") as fd:
+        pics = list()
+        try:
+            while True:
+                pics.append(readone(fd))
+        except:
+            pass
+
+        return pics
+
+
+hl = getpix("input.short")
+
+#print(hl[0])
+#print(rot90(rot270(hl[0])))
+
+
+
+
 
 
 
