dfe16782641c7010965cf5c77cea0114d9df4368
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-20 08:44:48 +0100

    reader for the data

--- 2020/20/20.py
+++ 2020/20/20.py
@@ -2,24 +2,38 @@
 def read(f):
     # parse one tile
 
-    h = readline(f)
-    f.strip('\n\r:')
-    myid = int(line.split[1])
+    h = f.readline()
+    h = h.strip('\n\r:')
+    myid = int(h.split()[1])
 
     A = list()
     # then read the 10 lines
-    for i in range(0.10):
-        A.append.readline(f).strip('\n\r').replace("#","1").replace(".","0")
+    for i in range(0,10):
+        A.append(f.readline().strip('\n\r').replace("#","1").replace(".","0"))
 
     # process a signature for each tile
     top = int(A[0],2)
     bottom = int(A[9],2)
-    left = int([x[9] for x in A],2)
-    right = int([x[0] for x in A],2)
+    left = int("".join([x[9] for x in A]),2)
+    right = int("".join([x[0] for x in A]),2)
 
-    return (id, top,bottom,left,right)
-
-fd = open("input.short","r")
-print(read(fd))
+    # and discard final empty line
+    f.readline()
     
+    return (myid, top,bottom,left,right)
+
+with open("input.short","r") as fd:
+    pics = list()
+    try:
+        while True:
+            pics.append(read(fd))
+    except:
+        pass
+
+    print(pics)
+
+
+
+
+
     
