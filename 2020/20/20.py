732c2df460ec67539db68d872d471d0ffa5b28f1
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-20 11:12:51 +0100

    need to change a few data types

--- 2020/20/20.py
+++ 2020/20/20.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+
+import copy
+import math
+
+from pprint import pprint
 
 def readone(f):
     # parse one tile
@@ -74,12 +80,55 @@ hl = getpix("input.short")
 #print(hl[0])
 #print(rot90(rot270(hl[0])))
 
+def tlist(tile):
+
+    return [tile, rot90(tile), rot180(tile), rot270(tile), flipvert(tile), fliphoriz(tile)]
+
+def ismatch(ta, tb):
+    #(myid, top,bottom,left,right,rtop,rbottom,rleft,rright) = tile
+    
+    return (ta[1]==tb[2] or ta[2] == tb[1] or ta[3] == tb[4] or ta[4] == tb[3])
+
+def matches(tile, hl):
+
+    if len(hl) == 0:
+        return []
+
+    ml = list()
+    
+    for i in hl:
+
+        ri = tlist(i)
+
+        for j in ri:
+
+            if ismatch(tile, j):
+                ml.append(j)
+
+    return (ml)
 
 
 
+print(hl[0])
+ml = list()
 
+for i in hl:
+    x = copy.copy(hl)
+    x.remove(i)
+    p = matches(i,x)
+    ml.append((i,len(p),p))
+    
 
+ml = sorted(ml, key=lambda x:-x[1])
+#pprint (ml)
 
+psize = int(math.sqrt(len(ml)))
+middle= psize // 2 + 1
+print ("PSize: "+str(psize)+" x "+str(psize) + ", c = ("+str(middle)+", "+str(middle)+")")
+
+print(ml[0][0])
+
+populatematrix(middle, middle, ml[0][0])
 
 
     
