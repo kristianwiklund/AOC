de6984cdf1ff67313d631557207d4560515a4f30
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-22 13:26:34 +0100

    2020 22 part 2 done

--- 2020/22/t.erl
+++ 2020/22/t.erl
@@ -35,14 +35,24 @@ round({[P1|P1S],[P2|P2S]}, GameID) ->
     L1 = length(P1S),
     L2 = length(P2S),
     if
-	(L1>=P1S) and (L2>=P2S) ->
-	    game({P1S,P2S}, #{}, GameID+1, 1);
+	(L1>=P1) and (L2>=P2) ->
+%	    io:fwrite("New Game ~p~n", [GameID+1]),
+	    X = game({lists:sublist(P1S,P1),lists:sublist(P2S,P2)}, #{}, GameID+1, 1),
+	    case X of
+		{p1win,_} ->
+		    {p1win, {P1S++[P1,P2],P2S}};
+		{p2win,_} ->
+		    {p1win, {P1S,P2S++[P2,P1]}}
+	    end;
+	
 	true -> 
 	    if 
 		P1>P2 ->
 		    {p1win, {P1S++[P1,P2],P2S}};
-		P2<P1 ->
-		    {p1win, {P1S,P2S++[P2,P1]}}
+		P1<P2 ->
+		    {p1win, {P1S,P2S++[P2,P1]}};
+		true ->
+		    io:fwrite("~p ~p~n",[[P1|P1S],[P2|P2S]])
 	    end
     end.
 
@@ -50,12 +60,21 @@ round({[P1|P1S],[P2|P2S]}, GameID) ->
 % check round before playing round to see if the round had been played before
 
 game(Deck, Rounds, GameID, RoundID) ->
+%    io:fwrite("Game ~p Round ~p ~p~n", [GameID, RoundID, Deck]),
     CheckDeck = maps:is_key(Deck, Rounds),
     NewState =
 	if CheckDeck ->
 		{p1win, Deck};
 	   true ->
-		round(Deck, GameID)
+		% need to check if any of the decks are empty
+		{_,V}=round(Deck, GameID),
+		{V1,V2}=V,
+		case {V1,V2} of
+		    {[],_}-> {p2win, V};
+		    {_,[]}-> {p1win, V};
+		    V->
+			game(V, maps:put(Deck, true, Rounds), GameID, RoundID+1) 
+		end	    
 	end,
     NewState.
 
