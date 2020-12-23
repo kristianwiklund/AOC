aab3b68e4c4766497c3650bc7f64564a726ebebf
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-23 04:53:24 +0100

    2020 20 done

--- 2020/20/hunt.sh
+++ 2020/20/hunt.sh
@@ -1,33 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 
+function f() {
+    cat $1 | tr -d "\n" > flat
+
+#echo $BOP
+# without monster
+
+    # the sed fails since it has more than 9 backrefernces
+    echo "" >s2
+    echo "--- sea ---">>s2
+    cat flat | ./notsed.py "$BOP" | sed -E "s/(.{96})/\1\n/g" | grep -v '^$' >>s2
+#    echo $1
+   cat flat | ./notsed.py "$BOP" | sed 's/\(.\)/\1\n/g' | sort | uniq -c | grep "#"
+
+# 2443 is too high
+}
 #echo "Hunting the monster"
 sed -e '1,/--- sea ---/ d' | tr '01' ' #' > sea
-cat sea | rev > searev
-./rot.py < sea > searot
-cat searot | rev > searotrev
-./rot.py < searev > searevrot
-cat searot | ./rot.py > searotrot
-cat searotrot | ./rot.py > searotrotrot
-cat searotrot | rev > searotrotrev
+#cat sea | rev > searev
+#./rot.py < sea > searot
+#cat searot | rev > searotrev
+#./rot.py < searev > searevrot
+#cat searot | ./rot.py > searotrot
+#cat searotrot | ./rot.py > searotrotrot
+#cat searotrot | rev > searotrotrev
 
 # searotrev is the match in the example btw
 
-MAX=`tail -1 sea | wc -c`
+MAX=`cat sea | wc -l`
+let MAX+=8
+echo $MAX
 
 # to get the monsters to the right place, we need to put them on MAX characters distance _start to start_"
 M1="#(."
 M2=")#(....)##(....)##(....)###("
 M3=".)#(..)#(..)#(..)#(..)#(..)#"
-M2x=`printf %104s $M2 | tr ' ' '.'` #24 + number of (
-M3x=`printf %104s $M3 | tr ' ' '.'` #21 +number of ( 
-
-cat searotrotrot | tr -d "\n" > flat
+M2x=`printf %${MAX}s $M2 | tr ' ' '.'` #24 + number of (
+#M2x=`printf %104s $M2 | tr ' ' '.'` #24 + number of (
+M3x=`printf %${MAX}s $M3 | tr ' ' '.'` #21 +number of (
+#M3x=`printf %104s $M3 | tr ' ' '.'` #21 +number of ( 
 BOP="($M1$M2x$M3x)"
-#echo $BOP
-# without monster
 
-# the sed fails since it has more than 9 backrefernces
-cat flat | ./notsed.py "$BOP" | sed -E "s/(.{96})/\1\n/g" 
-cat flat | ./notsed.py "$BOP" | sed 's/\(.\)/\1\n/g' | sort | uniq -c
-
-# 2443 is too high
+#f searotrot
+#f searotrotrot
+#f searotrotrev
+#f searevrot
+f sea>/dev/null
+f s2>/dev/null
+f s2>/dev/null
+f s2
+#f searot
+#f searotrev
