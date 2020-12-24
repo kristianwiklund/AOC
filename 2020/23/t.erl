c37643b85bb2cdcf3b574675bb9f107eb5ce3afc
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-24 02:39:05 +0100

    save

--- 2020/23/t.erl
+++ 2020/23/t.erl
@@ -1,5 +1,10 @@
 -module(t).
--export([move/3,chopout/2, findinsertcup/4, newcup/2,tt/0,t/0]).
+-export([move/4,chopout/2, findinsertcup/3, newcup/3,tt/0,t/0,t2/1]).
+
+% original: 53350253
+% putting pile first: 20429870 (see comment below)
+% removing unnecessary calls to length: 18754332
+% removing max/min:10493432
 
 chopout(L, I) ->
     {L1,L2} = lists:split(I, L),
@@ -13,31 +18,28 @@ chopout(L, I) ->
 		end,
     {L1P,L2P}.
 
+findinsertcup(Cup, Pile, Circle) when Cup<0 ->
+    findinsertcup(lists:max(Circle), Pile, Circle);
 
-findinsertcup(Cup, Min, Max, Circle) when Cup<Min ->
-    findinsertcup(Max, Min, Max, Circle);
-findinsertcup(Cup, Min, Max, Circle) ->
+
+findinsertcup(Cup, Pile, Circle) ->
 
     X = string:str(Circle, [Cup]),
-    
     if
 	X==0 ->
-	    findinsertcup(Cup-1, Min, Max, Circle);
+	    findinsertcup(Cup-1, Pile, Circle);
 	true ->
 	    Cup
     end.
 
-
-
 insert(Circle, Pile, Where) ->
     I = string:str(Circle, [Where]),
     {L1,L2} = lists:split(I, Circle),
-    L1++Pile++L2.
+    
+    Pile++(L2++L1). % putting the pile in the front is a 50% speedup
 
-newcup(Circle, Cup) ->
+newcup(Circle, Cup, LC) ->
     X = string:str(Circle, [Cup]),
-    LC = length(Circle),
-
     I=if
 	X==LC ->
 	    1;
@@ -45,25 +47,38 @@ newcup(Circle, Cup) ->
       end,
     lists:nth(I, Circle).
 
+%% change to not cut paste insert, do cut insert in the same move(?)
+
     
-move (Circle, Cup, 0) ->
+move (Circle, Cup, 0, _) ->
     I = string:str(Circle,[1]),
     {L1,L2} = lists:split(I, Circle),
     L = L2++L1,
-    {L, Cup};
-move (L, Cup, MaxMoves) ->
-    io:fwrite("~p: ~p ~p ~n",[MaxMoves, Cup, L]),
+    {Cup, L};
+move (L, Cup, MaxMoves, Len) ->
     I = string:str(L, [Cup]),
+% these three can 
     {Circle, Pile} = chopout(L, I),
-    InsertCup = findinsertcup(Cup-1, lists:min(Circle), lists:max(Circle), Circle),
+    InsertCup = findinsertcup(Cup-1, Pile, Circle),
     T2 = insert(Circle, Pile, InsertCup),
-    NewCup = newcup(T2, Cup),
-%    T2.
-    move(T2, NewCup, MaxMoves-1).
+% be combined for speedup
+    NewCup = newcup(T2, Cup, Len),
+    move(T2, NewCup, MaxMoves-1, Len).
 
 
 t() ->
-    move(i:d(),1,100).
+    move(i:d(),1,100,length(i:d())).
 
 tt() ->
-    move(i:s(),3,100).
+    move(i:s(),3,100, length(i:s())).
+
+t2(MAX) ->
+    Len=1000000,
+    CSize=1000,
+    L = i:s(),
+    M = lists:max(i:s())+1,
+    P = lists:seq(M,CSize),
+    {Time,R} = timer:tc(t,move, [L++P, 1, MAX, Len, CSize]),
+    {Time,R}.
+
+    
