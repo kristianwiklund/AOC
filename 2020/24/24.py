d846226e95b405c0b0518897663c1349315c1ea4
Kristian Wiklund <githubkristian@snabela.nl>
2020-12-25 01:57:58 +0100

    2020 24 done

--- 2020/24/24.py
+++ 2020/24/24.py
@@ -42,8 +42,49 @@ def movezor(x,y,grid, line):
         grid.pop((x,y))
     else:
         grid[(x,y)] = "black"
+
+# find all black surrounding (x,y)
+def bctb(x, y, grid):
+
+    black = list()
+    white = list()
+    
+    for (dx,dy) in M.values():
+        if (dx+x,dy+y) in grid:
+            black.append((dx+x,dy+y))
+        else:
+            white.append((dx+x,dy+y))
+
+    return (black,white)
+        
     
 
+def flipzor(grid):
+
+    new = dict()
+    cnt = dict()
+    
+    for (x,y) in grid:
+        (black,white) = bctb(x,y, grid)
+
+        # keep as black
+        if len(black)==1 or len(black)==2:
+            new[x,y]="black"
+            
+        for i in white:
+            if i in cnt:
+                cnt[i]=cnt[i]+1
+            else:
+                cnt[i]=1
+                
+    for i in cnt:
+        if cnt[i]==2:
+            new[i]="bläck"
+
+    return (new)
+        
+    
+        
 # ---- "main" ----
 grid = dict()
 
@@ -52,9 +93,18 @@ lines = fd.readlines()
 
 for line in lines:
     line = line.strip("\n\r")
-
-    # eat the line
-
     movezor(0,0,grid, line)
 
-print(len(grid))
+print("Answer to part 1: "+str(len(grid)))
+
+# --- part 2 ---
+
+new = grid
+for i in range(0,100):
+    new=flipzor(new)
+
+print("Answer to part 2: "+str(len(new)))
+
+
+
+
