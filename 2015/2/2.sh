b4fe0db635494acebe8ab3ff2b6823b2ac228dd2
Kristian Wiklund <githubkristian@snabela.nl>
2021-01-04 00:43:56 +0100

    2015/2

--- 2015/2/2.sh
+++ 2015/2/2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo -n "Task 1: "
 (echo "0";for i in `cat input`; do (echo $i | tr 'x' '\n'|sort -n|tr '\n' ' '|awk '{print $1,$2,"*",$1,$3,"*",$2,$3,"*","+","+","2 *",$1,$2,"* + +"}'); done; echo "p") | dc
+echo -n "Task 2: "
+(echo "0";for i in `cat input`; do (echo $i | tr 'x' '\n'|sort -n|tr '\n' ' '|awk '{print $1,$1,"+",$2,$2,"+",$1,$2,$3,"* * ","+","+ +"}'); done; echo "p") | dc
