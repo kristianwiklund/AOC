10d09feb6e0c24452284256eac44b93574777bd3
Kristian Wiklund <githubkristian@snabela.nl>
2021-01-04 13:00:17 +0100

    2015 3 a

--- 2015/3/3a.sh
+++ 2015/3/3a.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DX[1]=1
+echo -n "Part 1: "
+(DX[1]=1
 DX[2]=-1
 DX[3]=0
 DX[4]=0
@@ -11,4 +12,4 @@ DY[4]=-1
 X=0
 Y=0
 echo $X $Y
-sed 's/\(.\)/\1\n/g' < input  | tr '<>^v' '1234' | while read a; do let X=$X+${DX[$a]}; let Y=$Y+${DY[$a]}; echo $X $Y; done
+sed 's/\(.\)/\1\n/g' < input  | tr '<>^v' '1234' | while read a; do let X=$X+${DX[$a]}; let Y=$Y+${DY[$a]}; echo $X $Y; done) | sort -u | wc -l
