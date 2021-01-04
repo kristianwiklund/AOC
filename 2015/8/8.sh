fc9d3e0156611d86695fabb8f26b9a3a03843e90
Kristian Wiklund <githubkristian@snabela.nl>
2021-01-04 16:29:04 +0100

    2015 8 both days

--- 2015/8/8.sh
+++ 2015/8/8.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# count the encoded size
-
-echo -n "Encoded size: "
 ES=`cat $1 | grep -v "^$" | tr -d '\n' |  wc -c`
-echo $ES
 
-# decode
-
-echo -n "Decoded size: "
 cat $1 | grep -v "^$" | sed 's/^"\(.*\)"$/\1/' > bop
 read DS < <(for i in `cat bop`; do printf $i; done | wc -c)
 
-echo $DS
+echo -n "Answer 1: "
+expr $ES - $DS
 
-echo -n "Difference: "
-expr $ES - $DS 
+read PC < <(for i in `cat $1`; do printf "\"%q\"" "$i" ; done| wc -c)
+
+
+echo -n "Answer 2: "
+expr $PC - $ES
