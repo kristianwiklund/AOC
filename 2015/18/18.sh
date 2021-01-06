63d9eea80edb2d09bc23d03d173b8970c3b77325
Kristian Wiklund <githubkristian@snabela.nl>
2021-01-06 10:49:34 +0100

    save

---
+++ 2015/18/18.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "char *input[] = {" > input.h
+cat $1 | sed -e's/^/"/' -e 's/$/",/' >> input.h
+echo "}" >> input.h
+
+S=`wc -l < $1`
+echo "#define XMAX $S" >> input.h
+echo "#define YMAX $S" >> input.h
---
+++ 2015/18/life.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include "input.h"
+
+void run() {
+
+  
+  
+  for(int y=0;y<YMAX;y++) {
+
+    for(int x=0;x<XMAX;x++) {
+
+      if(test(x,y,))
+	
+      
+    }
+    
+  }
+
+}
+
+int main() {
+
+  
+  
+  
+}
+
