5d0b0e47f8163a833d0dc712ce2f422b73a038f5
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-01 07:08:11 +0100

    2021 day 1

--- 2021/1/1b.py
+++ 2021/1/1b.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
 import fileinput
+import rolling
 
-t=None
+t=False
+l=list()
 c=0
 for line in fileinput.input():
-    if t and int(line)>t:
-        c+=1
-    t = int(line)
-print(c)
+    l.append(int(line))
+
+rl = list(rolling.Sum(l,3))
+
+zl = list(zip(rl[:-1],rl[1:]))
+
+def c(t):
+      return 1 if t[0]<t[1] else 0
+
+print(sum(list(map(c,zl))))
+
