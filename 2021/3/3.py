a03d3de844652abfd279439c3511b25877b1f69b
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-03 20:04:44 +0100

    refactored

--- 2021/3/3.py
+++ 2021/3/3.py
@@ -12,35 +12,20 @@ def tp(X):
 g = int("".join(tp(X)),2)
 e= (2**(len(X[0]))-1)-g
 
-print (g*e)
+print ("A:",g*e)
 
-V = X
-p = ""
-c=0
-for i in range(len(V)):
-    m = tp(V)[c]
-    V=list(filter(lambda x: re.match(p+m,x),V))
-    p=p+"."
-    c=c+1
-    #print (V)
-    if len(V) == 1:
-        break
+def fp(V,T):
+    
+    p = ""
+    c=0
+    for i in range(len(V)):
+        m = T[tp(V)[c]]
+        V=list(filter(lambda x: re.match(p+m,x),V))
+        p=p+"."
+        c=c+1
+        if len(V) == 1:
+            return (V[0])
 
-oxgr = int(V[0],2)
-
-V = X
-p = ""
-c=0
-for i in range(len(V)):
-    m = tp(V)[c]
-    m = '1' if m=='0' else '0'
-    V=list(filter(lambda x: re.match(p+m,x),V))
-    p=p+"."
-    c=c+1
-    #print (V)
-    if len(V) == 1:
-        break
-
-cosr = int(V[0],2)
-
-print (cosr*oxgr)
+oxgr = int(fp(X,{"1":"1","0":"0"}),2)
+cosr = int(fp(X,{"1":"0","0":"1"}),2)
+print ("B:", cosr*oxgr)
