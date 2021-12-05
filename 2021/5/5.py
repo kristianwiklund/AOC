d12293be8abc4ca9ad51c6c3789a6f6824920d91
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-05 21:20:16 +0100

    2021-5 refactoring

--- 2021/5/5.py
+++ 2021/5/5.py
@@ -15,47 +15,36 @@ def pr(W):
             else:
                 print(int(y),end='')
         print("")
-        
+
+# --
+def draw(W,l):
+    dy = 1 if l["y2"]-l["y1"] > 0 else (-1 if l["y2"]-l["y1"] < 0 else 0)
+    dx = 1 if l["x2"]-l["x1"] > 0 else (-1 if l["x2"]-l["x1"] < 0 else 0)
+
+    x = l["x1"]
+    y = l["y1"]
+    
+    for i in range(max(abs(l["y2"]-l["y1"]),abs(l["x2"]-l["x1"]))+1):
+        W[x][y] = W[x][y] +1
+        y = y + dy
+        x = x + dx
+
+# --
 W = numpy.zeros(shape=(1000,1000))
 
-vertic = list(filter(lambda p: (p["x1"] == p["x2"]), points))
-horiz = list(filter(lambda p: (p["y1"] == p["y2"]), points))
-#print (horiz)
-#print (vertic)
+st = list(filter(lambda p: (p["x1"] == p["x2"]) or (p["y1"] == p["y2"]), points))
 
-for v in vertic:
-    maxy = max(v["y1"],v["y2"])
-    miny = min(v["y1"],v["y2"])
-
-    for i in range(miny, maxy+1):
-        W[v["x1"]][i] = W[v["x1"]][i] + 1
-
-for h in horiz:
-    maxx = max(h["x1"],h["x2"])
-    minx = min(h["x1"],h["x2"])
-
-    for i in range(minx,maxx+1):
-        W[i][h["y1"]] = W[i][h["y1"]] +1
-
+for p in st:
+    draw(W,p)
 
 print("Answer 1:",sum(sum(W>1)))
 
 # -- also lines with 45 degree angle
 
-dg = [item for item in points if item not in vertic]
-dg = [item for item in dg if item not in horiz]
-
+dg = [item for item in points if item not in st]
 
 for l in dg:
-
-    y = l["y1"]
-    dy = 1 if l["y2"]-l["y1"] > 0 else -1
-    dx = 1 if l["x2"]-l["x1"] > 0 else -1
-    
-    for x in range(l["x1"],l["x2"]+dx,dx):
-        W[x][y] = W[x][y] +1
-        y = y + dy
-
+    draw(W,l)
         
 print("Answer 2:",sum(sum(W>1)))
 
