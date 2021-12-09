94e543abd8724cb28ca81ba67587f4c15c6f233c
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-09 21:35:40 +0100

    2021 9

--- 2021/9/9.py
+++ 2021/9/9.py
@@ -2,6 +2,7 @@
 
 import sys
 from pprint import pprint
+import numpy
 
 f = [t.strip() for t in sys.stdin]
 
@@ -24,9 +25,6 @@ def ismin(M,x,y):
     c = c + (1 if y == 0 else 0)
     c = c + (1 if y == len(M[0])-1 else 0)
 
-    print(x,y,c)
-
-
     return c==4
 
 P = []
@@ -40,5 +38,38 @@ for x in range(len(M)):
             s=s+M[x][y]+1
     P.append(t)
 
-pprint(P)
 print("Answer 1:",s)
+
+# Where P is true, we have a low point
+
+def bh(X, T, x, y):
+
+    if x<0 or y<0:
+        return 0
+    if(x>=len(X) or y>=len(X[x])):
+        return 0
+    
+    if(T[x][y]):
+        return 0
+
+    if(X[x][y] == 9):
+        return 0
+    
+    T[x][y] = True
+    return bh(X, T, x-1, y) + bh(X, T, x+1, y) + bh(X, T, x, y-1) + bh(X, T, x, y+1)  + 1
+    
+def basin(X, x, y):
+    T = numpy.array(X)
+    T = T > 4711
+
+    return bh(X, T, x, y)
+
+t = [] 
+for x in range(len(M)):
+    for y in range(len(M[x])):
+        if P[x][y]:
+            t.append(basin(M, x, y))
+
+t = sorted(t,reverse=True)
+
+print("Answer 2:", t[0]*t[1]*t[2])
