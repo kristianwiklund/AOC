6eeaeab5fa82a3ebf204e2cf29da87ef2867200b
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-12 15:01:24 +0100

    2016 1

--- 2016/1/1.sh
+++ 2016/1/1.sh
@@ -8,3 +8,5 @@ grep P /tmp/blahonga > /tmp/bluhonga
 rm -f /tmp/blahonga
 sort /tmp/bluhonga  | uniq -c | grep -v "^ *1 " | sed 's/ *[0-9]* //' > /tmp/bohonga
 grep -f /tmp/bohonga /tmp/bluhonga | head -1 | awk '{print "Answer 2:",sqrt($2*$2)+sqrt($3*$3)}'
+rm -f /tmp/bohonga
+rm -f /tmp/bluhonga
