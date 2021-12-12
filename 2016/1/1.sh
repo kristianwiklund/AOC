a6652ed00532925118aa632240afc8a74f50e5dd
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-12 14:06:31 +0100

    refactor

--- 2016/1/1.sh
+++ 2016/1/1.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-cat input | tr -d ' '| tr ',' '\n' | sed -e 's/R/R /' -e 's/L/L /' | awk -f 1.awk
+tr -d ' '| tr ',' '\n' | sed -e 's/R/R /' -e 's/L/L /' | awk -f 1.awk > /tmp/blahonga
+echo -n "Answer 1: "
+grep -v P /tmp/blahonga
+rm -f /tmp/blahonga
