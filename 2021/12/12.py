22980c6a4cb6759e1d285ed7e040849054ab5b02
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-12 12:14:08 +0100

    x

---
+++ 2021/12/12.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import networkx as nx
+import sys
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+f = [t.strip().split("-") for t in sys.stdin]
+G = nx.Graph()
+
+for l in f:
+    G.add_edge(l[0],l[1])
+
+def fop(x):
+    return x.isupper() or x=="end"
+
+# remove nodes that cannot be visited at all
+
+remove = []
+for i in G:
+    if i == "end" or i == "start":
+        continue
+    N = nx.neighbors(G,i)
+
+    F = list(filter(fop,N))
+    if not len(F):
+        remove.append(i)
+
+nx.draw(G,with_labels=True)
+plt.savefig("nw.png")
+
+        
+for x in remove:
+    G.remove_node(x)
+
+        
+# cleaned up network, now traverse
+
+def visit(G, n, V):
+    if n=="end":
+        return []
+
+    if n in V:
+        print (n, " already visited")
+        return []
+
+    VV = V if n.isupper() else V|set([n])
+    
+    S = set(G.neighbors(n))-V
+    #print("Node",n,V,S)
+    P = []
+    for i in S:
+        if not i in V:
+            Z = visit(G,i,VV)
+            #for x in Z:
+            #    P.append(x)
+
+            if not Z:
+                P.append([i])
+            else:
+                for x in Z:
+                    Y = [i] + x
+                    P.append(Y)
+        else:
+            print("not doing",n,"->",i)
+    return (P)
+
+P=[]
+for i in G.neighbors("start"):
+    T = visit(G, i, set(["start"]))
+    for x in T:
+        Y = ["start",i] + x
+        P.append(Y)
+    
+pprint(P)
+print(len(P))
