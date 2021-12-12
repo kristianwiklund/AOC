0131c5105160efa72cf93a2771378439bafac620
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-12 18:12:50 +0100

    optimization

--- 2021/12/12.py
+++ 2021/12/12.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-
 import g as nx
 import sys
-#from pprint import pprint
-
 f = [t.strip().split("-") for t in sys.stdin]
 G = nx.Graph()
 
@@ -11,32 +8,32 @@ for l in f:
     G.add_edge(l[0],l[1])
 
 def visit(G, n, V, twice):
-    
+
     VV = V if n[0].isupper() else V|set([n])
     P = 0
     for i in G.neighbors(n):
         
         if i == "end":
-            P=P+1
-            continue
+            P+=1
         elif (not i in V):
-            Z = visit(G,i,VV, twice)
+            P+= visit(G,i,VV, twice)
         elif not twice and i!="start":
-            Z = visit(G,i,VV, True)
-        else:
-            continue
-        
-        P=P+Z
+            P+= visit(G,i,VV, True)
+
     return (P)
 
 def bolibompa(twice):
     
     c=0
+    Q=set(["start"])
     for i in G.neighbors("start"):
-        T = visit(G, i, set(["start"]), twice)
+        T = visit(G, i, Q, twice)
         c+=T
     return c
 
-print("Answer 1:",bolibompa(True))
-print("Answer 2:",bolibompa(False))
+print("Answer 1:",end='')
+print(bolibompa(True))
+print("Answer 2:",end='')
+print(bolibompa(False))
+
 
