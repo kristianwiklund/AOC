9da89247436145de08ea97012772ebf9c366e13e
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-12 15:00:49 +0100

    2016 1

--- 2016/1/1.awk
+++ 2016/1/1.awk
@@ -1,4 +1,4 @@
 BEGIN {dx=0;dy=-1;x=0;y=0}
-/R/ {t=dx;dx=-dy;dy=t;x=x+$2*dx;y=y+$2*dy;print "P", x,y}
-/L/ {t=dx;dx=dy;dy=-t;x=x+$2*dx;y=y+$2*dy;print "P", x,y}
+/R/ {t=dx;dx=-dy;dy=t;for(i=0;i<$2;i++){x=x+dx;y=y+dy;print "P", x,y}}
+/L/ {t=dx;dx=dy;dy=-t;for(i=0;i<$2;i++){x=x+dx;y=y+dy;print "P", x,y}}
 END {print sqrt(x*x)+sqrt(y*y)}
--- 2016/1/1.sh
+++ 2016/1/1.sh
@@ -3,4 +3,8 @@
 tr -d ' '| tr ',' '\n' | sed -e 's/R/R /' -e 's/L/L /' | awk -f 1.awk > /tmp/blahonga
 echo -n "Answer 1: "
 grep -v P /tmp/blahonga
+
+grep P /tmp/blahonga > /tmp/bluhonga
 rm -f /tmp/blahonga
+sort /tmp/bluhonga  | uniq -c | grep -v "^ *1 " | sed 's/ *[0-9]* //' > /tmp/bohonga
+grep -f /tmp/bohonga /tmp/bluhonga | head -1 | awk '{print "Answer 2:",sqrt($2*$2)+sqrt($3*$3)}'
