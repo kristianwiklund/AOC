d760a0878eedcfd81c778809dfbae3e034bdfc28
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-15 20:52:33 +0100

    2021 15

--- 2021/15/15.py
+++ 2021/15/15.py
@@ -29,4 +29,30 @@ P=nx.single_source_dijkstra(G,"0,0",str(len(M)-1)+","+str(len(M[0])-1))
 
 print("Answer 1:",P[0])
 
+G = nx.DiGraph()
+
+Mx=len(M[0])
+My=len(M)
+
+def w(M,x,y):
+    lx=len(M[0])
+    ly=len(M)
+    nx=x%lx
+    ny=y%ly
+    wx=x // lx
+    wy=y // ly
+#    print(x,y,nx,ny,wx,wy,M[ny][nx]+wx+wy)
+    return M[ny][nx]+wx+wy if M[ny][nx]+wx+wy <= 9 else M[ny][nx]+wx+wy-9
+    
+for y in range(len(M)*5):
+    for x in range(len(M[0])*5):
+        if x<len(M[0]*5)-1:
+            G.add_edge(str(x)+","+str(y),str(x+1)+","+str(y),weight=w(M,x+1,y))
+            G.add_edge(str(x+1)+","+str(y),str(x)+","+str(y),weight=w(M,x,y))
+        if y<len(M)*5-1:
+            G.add_edge(str(x)+","+str(y),str(x)+","+str(y+1),weight=w(M,x,y+1))
+            G.add_edge(str(x)+","+str(y+1),str(x)+","+str(y),weight=w(M,x,y))
+        
+P=nx.single_source_dijkstra(G,"0,0",str(len(M)*5-1)+","+str(len(M[0])*5-1))
+print("Answer 2:",P[0])
 
