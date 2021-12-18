1d6e8f7d6f46048574cfb3fa0376b3fbb30c646f
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-18 20:52:56 +0100

    intermediate

--- 2021/18/18.py
+++ 2021/18/18.py
@@ -8,19 +8,30 @@ IN = [eval(x.strip()) for x in sys.stdin]
 s=[[6,[5,[4,[3,2]]]],1]
 
 def lowboom(X):
-#    print("lowboom",X)
+
     if type(X[0])==list and type(X[1])!=list:
         return ([0,X[1]+X[0][1]],X[0][0],None) # combine list, carry the leftmost value
     if type(X[1])==list and type(X[0])!=list:
         return ([X[0]+X[1][0], 0], None, X[1][1]) # combine list, carry the rightmost valu
-    return (X, None, None)
+
+    if type(X[0]) != list and type (X[1])!=list:
+        return (X,None,None)
+
+    print("lowboom",X)
+    # these are two pairs
+    # the left one explodes, and is added to the pair to the right which replaces both pairs
+    LP = X[0]
+    RP = X[1]
+    
+    return ([RP[0]+LP[1],0], LP[0], RP[1])
 
 assert(lowboom([1,[2,3]])==([3,0],None, 3))
 assert(lowboom([[1,2],3])==([0,5],1,None))
-
+print(lowboom([[1,1],[2,2]]))
+print(lowboom([0, [5, 8]]))
 def notrightboom(X, nu):
 
-    #    print("NRB",X)
+    print("NRB",X,nu)
     if type(X) != list:
         return X
 
@@ -30,34 +41,42 @@ def notrightboom(X, nu):
     if type(X[1]) != list:
         return [X[0],X[1]+nu]
 
+    print("----- dragons")
+    print (X[0])
+    return [notrightboom(X[0],nu),X[1]]
 
 def boom(X,level):
     if type(X) != list: # should never end up here, but anyway...
+        print("wtf")
         return None
 
-    #print(level, X)
+    print(level, X)
     if level==3:
         R=lowboom(X)
-        #        print("lowboom",R)
+        print("lowboom",R)
         return R
     
     if type(X[0]) == list and type(X[1])!=list:
 
         (NewL, vl, vr) = boom(X[0],level+1)
         if vr:
-            return ([NewL, X[1]+vr], vl, None)
+            NewR=X[1]+vr
+            print("MR2",NewL, NewR)
+            return ([NewL, NewR], vl, None)
         else:
+            print("MR3",NewL, X[1])
             return ([NewL,X[1]], vl, vr)
 
     if type(X[0]) != list and type(X[1])==list:
 
         V = boom(X[1],level+1)
-        #print(V)
-        (NewL, vl, vr) = V
+        print(V)
+        (NewR, vl, vr) = V
+        print(NewR)
         if vl:
-            return ([X[0]+vl, NewL], None, vr)
+            return ([X[0]+vl, NewR], None, vr)
         else:
-            return ([X[0], NewL], vl, vr)
+            return ([X[0], NewR], vl, vr)
 
     if type(X[0]) != list and type(X[1])!= list:
         return (X, None, None)
@@ -67,6 +86,7 @@ def boom(X,level):
     (NewLL,vll, vrl) = boom(X[0],level+1)
     if vrl:
         NewLR = notrightboom(X[1],vrl)
+        print("nrb:",NewLR)
         return ([NewLL,NewLR],None,None)
     
     (NewLR,vlr, vrr) = boom(X[1],level+1)
@@ -109,6 +129,10 @@ assert(L==[[[[0,7],4],[[7,8],[6,0]]],[8,1]])
 
 def split(X,splitted=False):
 
+#    if not X:
+#        print( X)
+#        return (X,False)
+    
     if type(X)!=list:
         if X>9 and not splitted:
             return ([X//2,X-X//2], True)
@@ -145,7 +169,9 @@ def dothething(L):
     ap=True
 
     while ap:
+        print("boom", L)
         (L,nl,nr)=boom(L,0)
+        print("split", L)
         (L,ap)=split(L)
     return L
 
@@ -153,16 +179,8 @@ L=add([[[[4,3],4],4],[7,[[8,4],9]]],[1,1])
 L=dothething(L)
 assert(L==[[[[0,7],4],[[7,8],[6,0]]],[8,1]])
 
-# now we can do what we are supposed to do...
-
-L=False
-
-for i in IN:
-    if not L:
-        L = i
-    else:
-        L = add(L,i)
-        print(L)
-        L = dothething(L)
-
+V=add([[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]])
+L=dothething(V)
+print(V)
 print(L)
+assert(L==[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]])
