06fe2b93f1504c4f0b133006feb1054d6531af9a
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-19 14:59:51 +0100

    x

--- 2021/19/19.py
+++ 2021/19/19.py
@@ -16,83 +16,107 @@ for i in sys.stdin:
     elif len(j)==3:
         ipp[scanner].append(numpy.array([int(x) for x in j]))
 
-for i in ipp:
-    ipp[i] = numpy.array(ipp[i])
+print("all scanners loaded")
+#for i in ipp:
+#    ipp[i] = numpy.array(ipp[i])
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
 
-for i in ipp:
-    for j in ipp[i]:
-        ax.scatter(j[0], j[1], j[2], marker=i)
+#for i in ipp:
+#    for j in ipp[i]:
+#        ax.scatter(j[0], j[1], j[2], marker=i)
 
-plt.savefig("points.png")
+#plt.savefig("points.png")
 
 def klubba(A,B,mykt=False):
 
     bop = []
     gop = {}
+
     for i in A:
         for j in B:
             G = i-j
 
             s = (int(G[0]),int(G[1]),int(G[2]))
-            if mykt:
-                print (s)
+            
             if s in gop:
                 gop[s]+=1
             else:
                 gop[s]=1
 
-
+    if mykt:
+        from pprint import pprint
+        pprint(gop)
     for t in gop:
-
-        if gop[t]==12:
+        if gop[t]>=6:
             return t
             
     return None
             
+def rotate(A,m):
+    return (numpy.dot(A,m))
 
 def align(ipp,cpp):
     
-    for i in range(0,len(ipp)-1):
+    for i in range(len(ipp)):
         A = ipp[i]
-        for j in range(i+1,len(ipp)):
-            if (i,j) in cpp:
+
+        A=numpy.array(A)
+        
+        for j in range(i,len(ipp)):
+            if (i,j) in cpp or i==j:
                 continue
-
-            print ("trying "+str(i)+" vs "+str(j))
             B = ipp[j]
+            
+            B = numpy.array(B)
+            print ("trying "+str(i)+" vs "+str(j))
 
+            for x in [0,90,180,270]:
+                for y in [0,90,180,270]:
+                    for z in [0,90,180,270]:
 
-            for a in ["x","y","z"]:
-                for d in [0,90,180,270]:
-                    r = R.from_euler(a, d, degrees=True)
-                    m = r.as_matrix()
-                    m = m*((abs(m)>0.5))
-                    r = R.from_matrix(m)
-                    V=r.apply(B)
-                    mykt = (i==1) and (j==4)
-                    if mykt:
-                        print ("Trying "+str(i)+" and "+str(j)+" "+str(a)+"="+str(d))
-                        print(V-A)
-                    t = klubba(A,V,False)
-                    if t:
-                        print ("Match between "+str(i)+" and "+str(j)+" "+str(a)+"="+str(d))
-                        U=[]
-                        for tt in V:
-                            U.append(tt+t)
-                        ipp[j]=U
-                        cpp[(i,j)]="aligned"
-                        return True
+                        rx = R.from_euler("X", x, degrees=True)
+                        mx = rx.as_matrix()
+                        mx = mx*((abs(mx)>0.5))
+                        mx = mx.astype(int)
+                        
+                        ry = R.from_euler("Y", y, degrees=True)
+                        my = ry.as_matrix()
+                        my = my*((abs(my)>0.5))
+                        my = my.astype(int)
+                        
+                        rz = R.from_euler("Z", z, degrees=True)
+                        mz = rz.as_matrix()
+                        mz = mz*((abs(mz)>0.5))
+                        mz = mz.astype(int)
+                        
+                        V=rotate(B,mx)
+                        V=rotate(V,my)
+                        V=rotate(V,mz)
+
+                        t = klubba(A,V)
+
+                        if t:
+                            print ("Match between "+str(i)+" and "+str(j)+ " for ("+str(x)+","+str(y)+","+str(z)+")")
+                            U=[]
+                            for tt in V:
+                                U.append(tt+t)
+                            print("Realign "+str(j))
+                            ipp[j]=U
+                            cpp[(i,j)]="aligned"
+                            return True
+
     return False
 
 cpp={}
+
 while(align(ipp,cpp)):
-    print("bopp")
+    pass
 
+p = set()
+for i in ipp:
+    for j in ipp[i]:
+        p.add((j[0],j[1],j[2]))
 
-#r = R.from_euler('y', 180, degrees=True)
-#B=numpy.matrix([[686,422,578]])
-#print(B)
-#print(r.apply(B))
+print(p,len(p))
