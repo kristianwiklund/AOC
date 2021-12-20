207403d7b78447b6b50bb4a1a33ed402924d1c92
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-20 21:34:15 +0100

    x

--- 2021/18/d18.py
+++ 2021/18/d18.py
@@ -58,15 +58,33 @@ def explode(s):
 
             (startr,endr)=match.span()
             value=int(w[startr:][0])
-            w = w[:startr]+str(value+l)+w[startr+1:]
 
+            w = w[:startr]+(str(value+l)[::-1])+w[startr+1:]
+        
             w=w[::-1]
-
+            
             s = w + s[where-1:]
 
-
-
     return s
+
+def add(x,y):
+    return "["+x+","+y+"]"
+
+def split(s):
+
+    match = re.search(number+number,s)
+    if not match:
+        return s
+
+    (start,end)=match.span()
+    #print(start,end)
+    v = int(s[start:end])
+
+    return s[:start]+"["+str(v//2)+","+str(v-v//2)+"]"+s[end:]
+
+# ------------- test cases ---------------
+
+# explode
 
 try:
     s1="[[[[[9,8],1],2],3],4]"
@@ -103,3 +121,60 @@ except:
     print (explode(s5))
     print("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
     sys.exit()
+
+# add
+    
+assert(add("[1,2]","[[3,4],5]")=="[[1,2],[[3,4],5]]")
+assert(add("[[[[4,3],4],4],[7,[[8,4],9]]]","[1,1]")=="[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
+assert(add(add(add("[1,1]","[2,2]"),"[3,3]"),"[4,4]")=="[[[[1,1],[2,2]],[3,3]],[4,4]]")
+
+# split
+
+assert(split("[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")=="[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]")
+assert(split("[[[[0,7],4],[15,[0,13]]],[1,1]]")=="[[[[0,7],4],[[7,8],[0,13]]],[1,1]]")
+
+s = add("[[[[4,3],4],4],[7,[[8,4],9]]]","[1,1]")
+assert (s=="[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]")
+s = explode(s)
+assert (s=="[[[[0,7],4],[7,[[8,4],9]]],[1,1]]")
+s = explode(s)
+try:
+    assert (s=="[[[[0,7],4],[15,[0,13]]],[1,1]]")
+except:
+    print(s)
+    sys.exit()
+s = split(s)
+s = split(s)
+s = explode(s)
+assert (s == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+
+# ------------
+
+def red(s):
+
+    ts = s
+    os = ""
+    
+    while os != s:
+        os = s
+        s = explode(s)
+
+    if s == ts:
+        return s
+    
+    os = ""
+    while os != s:
+        os = s
+        s = split(s)
+
+
+    os = ""
+    while os != s:
+        os = s
+        s = red(s)
+        
+    return s
+
+assert (red("[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]") == "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]")
+
+# ---------------
