55b3b368f0f9589d4c45ea4e61aa4f20c90054e3
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-20 21:09:59 +0100

    x

--- 2021/18/d18.py
+++ 2021/18/d18.py
@@ -1,29 +1,50 @@
 #!/usr/bin/python3
 
 import re
-
+import sys
 
 
 level="\[[^\[\]]*"
+fish="\[[0-9],[0-9]\]"
 number="[0-9]"
-comma=","
+
+def whot(s):
+    l=0
+    c=0
+    for i in s:
+        c+=1
+        if i=="[":
+            l+=1
+            if l==5:
+                match = re.search(fish,s[c-1:])
+                (start,end)=match.span()
+                c = c + start
+                return c
+        if i=="]":
+            l-=1
+
+    return None
 
 def explode(s):
-    match = re.search(level+level+level+level+number+comma+number,s)
-
-    if match:
-        #print(match)
-        (start, end)=match.span()
-        where = end-3
+    restr = level+level+level+level+fish
+    #print(restr)
+    #match = re.search(restr,s)
+    where = whot(s)
+    #print ("wh",s,s[where:])
+    if where:
+        
+        #print ("slajs",s[where:where+3])
+        start = where
+        end = where+3
         (l,r)=s[where:where+3].split(",")
         #print(l,r)
         l = int(l)
         r = int(r)
         
         # find the first number to the right of the hit
-
-        match = re.search(number,s[end:])
+        match = re.search(number,s[where+3:])
         if match:
+            #print (s)
             #print(s[end:])
             #print (match)
             (startr,endr)=match.span()
@@ -56,20 +77,42 @@ def explode(s):
             #print ("spre",s)
             s = w + s[where-1:]
             #print("spost",s)
-        return s
-        
-s1="[[[[[9,8],1],2],3],4]"
-assert (explode(s1) == "[[[[0,9],2],3],4]")
 
-s2="[7,[6,[5,[4,[3,2]]]]]"
-assert(explode(s2) == "[7,[6,[5,[7,0]]]]")
 
-s3="[[6,[5,[4,[3,2]]]],1]"
-assert (explode(s3) == "[[6,[5,[7,0]]],3]")
+    return s
 
-s4="[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
 try:
-    assert (explode(s4) == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+    s1="[[[[[9,8],1],2],3],4]"
+    assert (explode(s1) == "[[[[0,9],2],3],4]")
 except:
-    print(explode(s4))
-    print ("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+    print (explode(s1))
+    print ("[[[[0,9],2],3],4]")
+    sys.exit()
+    
+try:
+    s2="[7,[6,[5,[4,[3,2]]]]]"
+    assert(explode(s2) == "[7,[6,[5,[7,0]]]]")
+except:
+    print (explode(s2))
+    print ( "[7,[6,[5,[7,0]]]]")
+    sys.exit()
+    
+try:
+    s3="[[6,[5,[4,[3,2]]]],1]"
+    assert (explode(s3) == "[[6,[5,[7,0]]],3]")
+except:
+    print(explode(s3))
+    print("[[6,[5,[7,0]]],3]")
+    sys.exit()
+    
+s4="[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]"
+assert (explode(s4) == "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+
+try:
+    s5="[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]"
+    assert(explode(s5) == "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+except:
+    print (s5)
+    print (explode(s5))
+    print("[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+    sys.exit()
