3a146190dfc8d69aa0d4826aad9989b54a34f2b5
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-22 19:17:41 +0100

    x

--- 2021/22/22.py
+++ 2021/22/22.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Box:
     def __init__(self, l):
@@ -19,15 +22,16 @@ class Box:
         
             (l,r)=splitinaTOR(y[0])
             self.x1 = l
-            self.x2 = r
+            self.x2 = r+1
         
-            (l,r)=splitinaTOR(y[0])
+            (l,r)=splitinaTOR(y[1])
             self.y1 = l
-            self.y2 = r
+            self.y2 = r+1
         
-            (l,r)=splitinaTOR(y[0])
+            (l,r)=splitinaTOR(y[2])
             self.z1 = l
-            self.z2 = r
+            self.z2 = r+1
+
         elif type(l)==list:
 
             self.state = l[0]
@@ -39,10 +43,10 @@ class Box:
             self.z2 = l[6]
 
     def __repr__(self):
-        return self.state+" x="+str(self.x1)+".."+str(self.x2)+",y="+str(self.y1)+".."+str(self.y2)+",z="+str(self.z1)+".."+str(self.z2)
+        return self.state+" x="+str(self.x1)+".."+str(self.x2-1)+",y="+str(self.y1)+".."+str(self.y2-1)+",z="+str(self.z1)+".."+str(self.z2-1)
 
     def size(self):
-        return (self.x2-self.x1+1)*(self.y2-self.y1+1)*(self.z2-self.z1+1)
+        return (self.x2-self.x1)*(self.y2-self.y1)*(self.z2-self.z1)
 
 class Reactor:
     def __init__(self):
@@ -59,53 +63,62 @@ class Reactor:
 
         # regardless of how we do this, we remove "cube" from "c", then we either add cube or not, depending on if it is on or off
 
-        # layer 1
-        L.append(Box(["on",c.x1,cube.x1,c.y1,cube.y1,cube.z2,c.z2])) # A
-        print(L)
-        L.append(Box(["on",cube.x1, cube.x2, c.y1,cube.y1,cube.z2,c.z2])) # B
-        L.append(Box(["on",cube.x2, c.x2, c.y1,cube.y1,cube.z2,c.z2])) # C
+        layer1 = True
+        layer2 = True
+        layer3 = True
         
-        L.append(Box(["on",c.x1,cube.x1,cube.y1,cube.y2,cube.z2,c.z2])) # D
-        L.append(Box(["on",cube.x1, cube.x2, cube.y1,cube.y2,cube.z2,c.z2])) # E
-        L.append(Box(["on",cube.x2,c.x2,cube.y1,cube.y2,cube.z2,c.z2])) # F
-        
-        L.append(Box(["on",c.x1,cube.x1,cube.y2,c.y2,cube.z2,c.z2])) # G
-        L.append(Box(["on",cube.x1, cube.x2, cube.y2,c.y2,cube.z2,c.z2])) # H
-        L.append(Box(["on",cube.x2, c.x2, cube.y2,c.y2,cube.z2,c.z2])) # I
+        if layer1:
+            # layer 1
+            L.append(Box(["on",c.x1,cube.x1, c.y1,cube.y1, cube.z2,c.z2])) # A
+            L.append(Box(["on",cube.x1, cube.x2, c.y1,cube.y1, cube.z2,c.z2])) # B
+            L.append(Box(["on",cube.x2, c.x2, c.y1,cube.y1, cube.z2,c.z2])) # C
+            
+            L.append(Box(["on",c.x1,cube.x1,cube.y1,cube.y2, cube.z2,c.z2])) # D
+            L.append(Box(["on",cube.x1, cube.x2, cube.y1,cube.y2, cube.z2,c.z2])) # E
+            L.append(Box(["on",cube.x2,c.x2,cube.y1,cube.y2, cube.z2,c.z2])) # F
+            
+            L.append(Box(["on",c.x1,cube.x1,cube.y2,c.y2, cube.z2,c.z2])) # G
+            L.append(Box(["on",cube.x1, cube.x2, cube.y2,c.y2, cube.z2,c.z2])) # H
+            L.append(Box(["on",cube.x2, c.x2, cube.y2,c.y2, cube.z2,c.z2])) # I
 
-        # layer 2
-        L.append(Box(["on",c.x1,cube.x1,c.y1,cube.y1,cube.z1,cube.z2])) # A
-        L.append(Box(["on",cube.x1, cube.x2, c.y1,cube.y1,cube.z1,cube.z2])) # B
-        L.append(Box(["on",cube.x2, c.x2, c.y1,cube.y1,cube.z1,cube.z2])) # C
-        
-        L.append(Box(["on",c.x1,cube.x1,cube.y1,cube.y2,cube.z1,cube.z2])) # D
+        if layer2:
+            # layer 2
+            L.append(Box(["on",c.x1,cube.x1,c.y1,cube.y1, cube.z1,cube.z2])) # A
+            L.append(Box(["on",cube.x1, cube.x2, c.y1,cube.y1, cube.z1,cube.z2])) # B
+            L.append(Box(["on",cube.x2, c.x2, c.y1,cube.y1, cube.z1,cube.z2])) # C
+            
+            L.append(Box(["on",c.x1,cube.x1,cube.y1,cube.y2, cube.z1,cube.z2])) # D
+            
+            L.append(Box(["on",cube.x2,c.x2,cube.y1,cube.y2, cube.z1,cube.z2])) # F
+            
+            L.append(Box(["on",c.x1,cube.x1,cube.y2, c.y2, cube.z1,cube.z2])) # G
+            L.append(Box(["on",cube.x1,cube.x2, cube.y2,c.y2, cube.z1,cube.z2])) # H
+            L.append(Box(["on",cube.x2,c.x2, cube.y2,c.y2, cube.z1,cube.z2])) # I
 
-        L.append(Box(["on",cube.x2,c.x2,cube.y1,cube.y2,cube.z1,cube.z2])) # F
-        
-        L.append(Box(["on",c.x1,cube.x1,cube.y2,c.y2,cube.z1,cube.z2])) # G
-        L.append(Box(["on",cube.x1, cube.x2, cube.y2,c.y2,cube.z1,cube.z2])) # H
-        L.append(Box(["on",cube.x2, c.x2, cube.y2,c.y2,cube.z1,cube.z2])) # I
+        if layer3:
+            # layer 3
+            L.append(Box(["on",c.x1,cube.x1, c.y1,cube.y1, c.z1,cube.z1])) # A
 
-        # layer 3
-        L.append(Box(["on",c.x1,cube.x1,c.y1,cube.y1,c.z1,cube.z1])) # A
-        L.append(Box(["on",cube.x1, cube.x2, c.y1,cube.y1,c.z1,cube.z1])) # B
-        L.append(Box(["on",cube.x2, c.x2, c.y1,cube.y1,c.z1,cube.z1])) # C
-        
-        L.append(Box(["on",c.x1,cube.x1,cube.y1,cube.y2,c.z1,cube.z1])) # D
-
-        L.append(Box(["on",cube.x2,c.x2,cube.y1,cube.y2,c.z1,cube.z1])) # F
-        
-        L.append(Box(["on",c.x1,cube.x1,cube.y2,c.y2,c.z1,cube.z1])) # G
-        L.append(Box(["on",cube.x1, cube.x2, cube.y2,c.y2,c.z1,cube.z1])) # H
-        L.append(Box(["on",cube.x2, c.x2, cube.y2,c.y2,c.z1,cube.z1])) # I
+            L.append(Box(["on",cube.x1,cube.x2, c.y1,cube.y1, c.z1,cube.z1])) # B
+            L.append(Box(["on",cube.x2,c.x2, c.y1,cube.y1, c.z1,cube.z1])) # C
+            
+            L.append(Box(["on",c.x1,cube.x1, cube.y1,cube.y2, c.z1,cube.z1])) # D 
+            L.append(Box(["on",cube.x1, cube.x2, cube.y1,cube.y2, c.z1,cube.z1])) # E
+            L.append(Box(["on",cube.x2,c.x2, cube.y1,cube.y2, c.z1,cube.z1])) # F
+            
+            L.append(Box(["on",c.x1,cube.x1, cube.y2,c.y2, c.z1,cube.z1])) # G 
+            L.append(Box(["on",cube.x1,cube.x2, cube.y2,c.y2, c.z1,cube.z1])) #
+            L.append(Box(["on",cube.x2,c.x2, cube.y2,c.y2, c.z1,cube.z1])) # I
 
         #S = list(map(lambda x:x.size(),L))
         #print("Remaining of ",str(c)," is ",sum(S)," blocks")
-        #L = list(filter(lambda x:x.size()>0,L))
-                
-        #print(L,len(L))
+        X = list(filter(lambda x:x.size()>0,L))
+        print(X,len(X))
 
-        return L
+        #P = list(filter(lambda x:x.size()<=0,L))
+        #print(P,len(P))
+
+        return X
 
 
         
@@ -186,44 +199,86 @@ readinaTOR()
 
 # Tests
 
-assert(str(Box("on x=10..12,y=10..12,z=10..12"))=="on x=10..12,y=10..12,z=10..12")
-assert(Box("on x=10..12,y=10..12,z=10..12").size()==27)
+#assert(str(Box("on x=10..12,y=10..12,z=10..12"))=="on x=10..12,y=10..12,z=10..12")
 
-
-R = Reactor()
-R = R + Box("on x=1..3,y=1..3,z=1..3")
-R = R + Box("off x=1..1,y=1..1,z=1..1")
-print(R.realcubes.__repr__())
-
-sys.exit()
-
-
-# test case from AOC
-R = Reactor()
-
-R = R + Box("on x=10..12,y=10..12,z=10..12")
-
-assert(R.realcubes.__repr__()=="[on x=10..12,y=10..12,z=10..12]")
-assert(R.size()==27)
-
-# test case 1: add a cube that is the exact size as the existing cube in the reactor
-R = R + Box("on x=10..12,y=10..12,z=10..12")
-assert(R.size()==27)
-
-# test case 2: add a cube that is smaller on one side compared to the existing cube in the reactor
-R = R + Box("on x=11..12,y=10..12,z=10..12")
-print(R.size())
-assert(R.size()==27)
+# assert(Box("on x=10..12,y=10..12,z=10..12").size()==27)
 
 
 
-R = R + Box("on x=11..13,y=11..13,z=11..13")
-assert(R.size()==19+27)
+# # test case from AOC
+# R = Reactor()
 
-R = R + Box("off x=9..11,y=9..11,z=9..11")
-assert(R.size()==19+27-8)
+# R = R + Box("on x=10..12,y=10..12,z=10..12")
 
-R = R + Box("on x=10..10,y=10..10,z=10..10")
-assert(R.size()==39)
-print(R)
+# assert(R.realcubes.__repr__()=="[on x=10..12,y=10..12,z=10..12]")
+# assert(R.size()==27)
 
+# # test case 1: add a cube that is the exact size as the existing cube in the reactor
+# R = R + Box("on x=10..12,y=10..12,z=10..12")
+# assert(R.size()==27)
+
+# # test case 2: add a cube that is smaller on one side compared to the existing cube in the reactor
+# R = R + Box("on x=11..12,y=10..12,z=10..12")
+# assert(R.size()==27)
+
+
+
+# R = R + Box("on x=11..13,y=11..13,z=11..13")
+#print(R.size())
+
+#try:
+#    assert(R.size()==19+27)
+#except:
+#    print(R.size())
+#    print(R)
+#    print(R.realcubes.__repr__())
+#    sys.exit()
+    
+#R = R + Box("off x=9..11,y=9..11,z=9..11")
+#assert(R.size()==19+27-8)
+
+#R = R + Box("on x=10..10,y=10..10,z=10..10")
+#assert(R.size()==39)
+#print(R)
+
+# --- visual test
+
+# print("Viz test")
+# R = Reactor()
+# R = R + Box("on x=1..3,y=1..3,z=1..3")
+# R = R + Box("off x=3..3,y=1..1,z=3..3")
+
+
+
+# c=0
+# ax = plt.figure().add_subplot(projection='3d')
+
+# for i in R.realcubes:
+#     c+=1
+#     print(i.x1,i.x2-1,i.y1,i.y2-1,i.z1,i.z2-1)
+#     n_voxels = np.zeros((4,4,4), dtype=bool)
+#     for x in range(i.x1, i.x2):
+#         for y in range(i.y1, i.y2):
+#             for z in range(i.z1, i.z2):
+#                 n_voxels[x,y,z]=True
+#                 ax.voxels(n_voxels)
+#                 plt.savefig("reactor"+str(c)+".png")
+
+                    
+# #facecolors = np.where(n_voxels, '#FFD65DC0', '#7A88CCC0')
+# #edgecolors = np.where(n_voxels, '#BFAB6E', '#7D84A6')
+
+
+
+# n_voxels = np.zeros((4,4,4), dtype=bool)
+
+# for i in R.reactor:
+#     #    print(i.x1,i.x2-1,i.y1,i.y2-1,i.z1,i.z2-1)
+#     for x in range(i.x1, i.x2):
+#         for y in range(i.y1, i.y2):
+#             for z in range(i.z1, i.z2):
+#                 n_voxels[x,y,z]=(i.state=="on")
+                
+# ax = plt.figure().add_subplot(projection='3d')
+# ax.voxels(n_voxels)
+# plt.savefig("reactorMaster.png")
