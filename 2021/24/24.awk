18b63ba772a9cb441884b0aa5c7f092e6eaf815a
Kristian Wiklund <githubkristian@snabela.nl>
2021-12-26 17:03:07 +0100

    x

--- 2021/24/24.awk
+++ 2021/24/24.awk
@@ -1,6 +1,6 @@
 BEGIN {g=0;print "def f(s):\n   x=0\n   y=0\n   z=0\n   w=0\n"}
 
-/inp / {print "   "$2"=int(s["g"]);";g=g+1;}
+/inp / {print "   return (w,z,y,z)\n\ndef i"g"(s, w,x,y,z):\n   "$2"=s";g=g+1;}
 /add / {if ($3!=0) {print "   "$2"+="$3";"}}
 /mul / {if ($3!=0) {print "   "$2"*="$3";"} else {print "   "$2"=0;"}}
 /div / {print "   "$2"//="$3";"}
---
+++ 2021/24/f.py
@@ -0,0 +1,300 @@
+def f(s):
+   x=0
+   y=0
+   z=0
+   w=0
+
+   return (w,z,y,z)
+
+def i0(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=14;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=7;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i1(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=12;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=4;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i2(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=11;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=8;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i3(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-4;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=1;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i4(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=10;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=5;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i5(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=10;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=14;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i6(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=15;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=12;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i7(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-9;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=10;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i8(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-9;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=5;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i9(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=1;
+   x+=12;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=7;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i10(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-15;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=6;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i11(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-7;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=8;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i12(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x+=-10;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=4;
+   y*=x;
+   z+=y;
+   return (w,z,y,z)
+
+def i13(s, w,x,y,z):
+   w=s
+   x=0;
+   x+=z;
+   x%=26;
+   z//=26;
+   x=1 if x==w else 0
+   x=1 if x==0 else 0
+   y=0;
+   y+=25;
+   y*=x;
+   y+=1;
+   z*=y;
+   y=0;
+   y+=w;
+   y+=6;
+   y*=x;
+   z+=y;
+   return (w,x,y,z)
