74b4b4355111dbe0960385133c14117cc90b6d61
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-02 13:27:19 +0100

    2022 2:2§

--- 2022/2/2.py
+++ 2022/2/2.py
@@ -19,7 +19,6 @@ play["C"] = {
     "Z":3+3, # scissors, scissors, draw
     }
 
-    
 
 with open("input.txt","r") as fd:
 
@@ -30,4 +29,38 @@ with open("input.txt","r") as fd:
         s = line.split(" ")
         score = score + play[s[0]][s[1]]
 
-print(score)
+print("part 1:",score)
+
+decide = {}
+# rock
+decide["A"] = {
+    "X": 0+3, #lose, scissors
+    "Y": 3+1, # draw, rock
+    "Z": 6+2 # win, paper
+    }
+# paper
+decide["B"] = {
+    "X": 0+1, #lose, rock
+    "Y": 3+2, # draw, paper
+    "Z": 6+3 # win, scissors
+    }
+
+# scissors
+decide["C"] = {
+    "X": 0+2, #lose, paper
+    "Y": 3+3, # draw, scissors
+    "Z": 6+1 # win, rock
+    }
+
+
+with open("input.txt","r") as fd:
+
+    lines = fd.readlines()
+    score = 0
+    for line in lines:
+        line = line.strip()
+        s = line.split(" ")
+        score = score + decide[s[0]][s[1]]
+
+print("part 2:",score)
+        
