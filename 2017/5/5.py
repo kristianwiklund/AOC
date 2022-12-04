1baf17a357fa77b5a7cc5a04489b9c800b497812
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-04 08:55:03 +0100

    2017 5

--- 2017/5/5.py
+++ 2017/5/5.py
@@ -15,3 +15,19 @@ with open("input.txt","r") as fd:
 
     print("Part 1:",cnt)
         
+with open("input.txt","r") as fd:
+
+    program = [int(x) for x in fd.readlines()]
+
+    pc = 0
+    cnt = 0
+    
+    while pc >-1 and pc < len(program):
+
+        newpc = pc + program[pc]
+    
+        program[pc] += 1 if program[pc]<3 else -1
+        pc = newpc
+        cnt+=1
+
+    print("Part 2:",cnt)
