eb88f4fa444fab538b757c69f083cb9a10d7fcfc
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-04 08:27:31 +0100

    refactor

--- 2022/4/4.py
+++ 2022/4/4.py
@@ -6,19 +6,15 @@ with open("input.txt","r") as fd:
     cnt2=0
     for line in lines:
         ep = line.strip().split(",")
-        a=[int (x) for x in ep[0].split("-")]
-        b=[int (x) for x in ep[1].split("-")]
+        a1,a2=[int (x) for x in ep[0].split("-")]
+        b1,b2=[int (x) for x in ep[1].split("-")]
 
-        sa = set(range(a[0],a[1]+1))
-        sb = set(range(b[0],b[1]+1))
+        sa = set(range(a1,a2+1))
+        sb = set(range(b1,b2+1))
 
-        if sa.issubset(sb) or sb.issubset(sa):
-            cnt=cnt+1
+        cnt += 1 if sa.issubset(sb) or sb.issubset(sa) else 0
+        cnt2 += 0 if sa.isdisjoint(sb) else 1
 
-        if not sa.isdisjoint(sb):
-            cnt2=cnt2+1
-        
-        
     print("Part 1:",cnt)
     print("Part 2:",cnt2)
 
