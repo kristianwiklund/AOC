efecdfcc5c99de9e180c363a6f8f0962bb14f0da
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-05 12:36:17 +0100

    refactor

--- 2022/5/5.py
+++ 2022/5/5.py
@@ -38,7 +38,17 @@ def move(torn,n,f,t):
         torn[t].append(b)
         n-=1
     return torn
-        
+
+
+def move9001(torn,n,f,t):
+    l = list()
+    while n:
+        b = torn[f].pop()
+        l.append(b)
+        n-=1
+    l.reverse()
+    torn[t] = torn[t]+l
+    return torn
 
 with open("input.code","r") as fd:
 
@@ -51,7 +61,8 @@ with open("input.code","r") as fd:
         t = int(line[5])
 
         torn = move(torn,n,f,t)
-
+        tb = move9001(tb,n,f,t)
+        
 print("Part 1: ",end="")
 
 for i in range(1,10):
@@ -63,40 +74,12 @@ for i in range(1,10):
         
 print("")
 
-#-------
-
-torn = tb
-
-
-def move9001(torn,n,f,t):
-    l = list()
-    while n:
-        b = torn[f].pop()
-        l.append(b)
-        n-=1
-    l.reverse()
-    torn[t] = torn[t]+l
-    return torn
-        
-
-with open("input.code","r") as fd:
-
-    lines = [x.strip() for x in fd.readlines()]
-
-    for line in lines:
-        line = line.split(" ")
-        n = int(line[1])
-        f = int(line[3])
-        t = int(line[5])
-
-        torn = move9001(torn,n,f,t)
-
 print("Part 2: ",end="")
 
 for i in range(1,10):
-    torn[i].reverse()
-    if len(torn[i]):
-        print(torn[i][0].replace("[","").replace("]",""),end="")
+    tb[i].reverse()
+    if len(tb[i]):
+        print(tb[i][0].replace("[","").replace("]",""),end="")
     else:
         print("...",end="")
         
