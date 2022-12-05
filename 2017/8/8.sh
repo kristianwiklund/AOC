64ae0d8f738d817b32d6fc115fc5e7b4d3793092
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-05 18:45:28 +0100

    2017 8

--- 2017/8/8.sh
+++ 2017/8/8.sh
@@ -3,9 +3,10 @@
 I=input.txt
 
 cut -d' ' -f1 $I | sort -u | awk '{print $1"=0"}' > gen.py
-
-cat $I  | sed -e 's/dec/-/' -e 's/inc/+/' | awk '{print $1"=("$0,"else",$1")"}' >> gen.py
-echo -n "print(max(" >> gen.py
+echo "m=0">>gen.py
+cat $I  | sed -e 's/dec/-/' -e 's/inc/+/' | awk '{print $1"=("$0,"else",$1")";print "m=max(m,"$1")"}' >> gen.py
+echo -n "print(\"Part 1\",max(" >> gen.py
 cut -d' ' -f1 $I | sort -u | tr '\n' ',' >> gen.py
 echo "))">>gen.py
+echo "print(\"Part 2\",m)">>gen.py
 python3 gen.py
