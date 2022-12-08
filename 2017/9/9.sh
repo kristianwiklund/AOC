d878f8d6e48fd850ce0a0da725875f63b33e7cbb
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-08 10:16:43 +0100

    recover file

--- 2017/9/9.sh
+++ 2017/9/9.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo -n "Part 1: "
 export BC_LINE_LENGTH=10000
-sed 's|!.||g' | sed 's|<[^>]*>||g' | sed 's|[^{}]||g' | sed 's/{/c=c+1;/g' | sed 's/}/print " ";print c;c=c-1;/g' | sed 's/^/print "\\n";c=0;/' | tr ';' '\n'| bc -l  | sed 's/^ *//' |  sed 's/$/\n/' |  sed 's| |\n.+|g' | bc -l| tail -1
+cat input.txt | sed 's|!.||g' | sed 's|<[^>]*>||g' | sed 's|[^{}]||g' | sed 's/{/c=c+1;/g' | sed 's/}/print " ";print c;c=c-1;/g' | sed 's/^/print "\\n";c=0;/' | tr ';' '\n'| bc -l  | sed 's/^ *//' |  sed 's/$/\n/' |  sed 's| |\n.+|g' | bc -l| tail -1
+
 
