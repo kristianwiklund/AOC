4e8f3853771a686485369580618c923f28d8d25c
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-11 17:00:25 +0100

    2022 11 b

--- 2022/11/11b.py
+++ 2022/11/11b.py
@@ -1,162 +1,66 @@
 from pprint import pprint
-import sys
-sys.path.append("../..")
-from utilities import *
 
 def ap(fd):
 
     who = fd.readline().strip().split(" ")[1][:-1]
-    l = fd.readline().strip().split(":")
-    try:
-        items = [int(x) for x in l[1].split(",")]
-    except:
-        items = []
+    items = [int(x) for x in fd.readline().strip().split(":")[1].split(",")]
     op = fd.readline().strip().split("=")[1]
     test = int(fd.readline().strip().split(" ")[-1])
     iftrue = int(fd.readline().strip().split(" ")[-1])
     iffalse = int(fd.readline().strip().split(" ")[-1])
     fd.readline()
+    
+    return({"m":who,"i":items,"o":op,"t":test,"it":iftrue,"if":iffalse,"cnt":0})
 
-    return({"m":who,"i":items,"o":op,"t":test,"it":iftrue,"if":iffalse,"cnt":0,"tc":0,"fc":0})
 
-
-def inspect(apes):
+def inspect(apes,mod):
     
     for m in range(len(apes)):
-        for i in apes[m]["i"]:
-            apes[m]["cnt"]+=1
+        #print("Monkey",m)
 
-            old = items[i]["v"]
-            items[i]["ap"][m]+=1
-            new = eval(apes[m]["o"])%(19*23*13*17)
+        for i in apes[m]["i"]:
+            #print (" Monkey inspects an item with worry level of",i)
+            apes[m]["cnt"]+=1
+            
+            old = i
+            new = eval(apes[m]["o"])%mod
+            #print (" Worry level op",apes[m]["o"],"goes to",new)
             #new = new // 3
-            items[i]["v"]=new
-            #items[i]["f"]=("("+apes[m]["o"].replace("old",items[i]["f"])+")").replace(" ","")
+            #print (" Monkey gets bored with item. Worry level is divided by 3 to",new)
+            #print (" Current worry level is ",end="")
+            
             if new % apes[m]["t"]:
-                #print("A",i,"->",apes[m]["if"])
-                items[i]["ss"]+=str(apes[m]["if"])
-                apes[m]["fc"]+=1
-                apes[apes[m]["if"]]["i"].append(i)
+                #print ("not diviseble by",apes[m]["t"])
+                #print (" Item with worry level",new,"is thrown to monkey",apes[m]["if"])
+                apes[apes[m]["if"]]["i"].append(new)
             else:
-                #print("A", i,"->",apes[m]["it"])
-                items[i]["ss"]+=str(apes[m]["it"])
-                apes[m]["tc"]+=1
-                apes[apes[m]["it"]]["i"].append(i)
+                #print ("diviseble by",apes[m]["t"])
+                #print (" Item with worry level",new,"is thrown to monkey",apes[m]["it"])
+                apes[apes[m]["it"]]["i"].append(new)
         apes[m]["i"] = list()
 
+        #print("")
+        #for m in range(len(apes)):
+        #    print ("Monkey",m,"-",apes[m]["i"],"inspected items",apes[m]["cnt"],"times")
 
     return (apes)
 
-def pap(i,apes):
     
-    return [(x["cnt"],x["tc"],x["fc"]) for x in apes]
-
-
-
-
-
-with open("input.short","r") as fd:
+with open("input.txt","r") as fd:
 
     apes = list()
+    mod=1
     while True:
         try:
-            apes.append(ap(fd))
+            x = ap(fd)
+            apes.append(x)
+            mod*=x["t"]
         except:
             break
-    items = dict()
-    
-    for i in range(len(apes)):
-        ni = list()
-        for j in apes[i]["i"]:
-            nin = (i,j)
-            items[nin]=dict()
-            items[nin]["ap"]={x:0 for x in range(len(apes))}
-            items[nin]["v"]=j
-            items[nin]["f"]=str(j)
-            items[nin]["ss"]=""
-            items[nin]["first"]=str(i)
-            ni.append(nin)
-        apes[i]["i"]=ni
-
     for i in range(10000):
-        inspect(apes)
-        if not i%100:
-            print(i)
-        if i==9999:
-            print ("APES 1", [x["cnt"] for x in apes])
-            
-    #for x in range(1):
-    #    for i in sorted(items,key=lambda x:x[1]):
-    #        print(i,"=",items[i]["ap"])
-            
-    #apes = sorted(apes,key=lambda x:-x["cnt"])
-    #print("Part 2:", apes[0]["cnt"],apes[1]["cnt"],apes[0]["cnt"]*apes[1]["cnt"])
+        apes = inspect(apes,mod)
 
-    
-    for i in items:
-        #print ("---",i,"---")
-        #print (i,items[i]["ss"])
-        items[i]["lrs"]=lrs(items[i]["ss"])
-        where = items[i]["ss"].index(items[i]["lrs"])
-        prefix = items[i]["ss"][:where]
-        items[i]["prefix"]=list(items[i]["first"]+prefix)
-        print(i,prefix,items[i]["lrs"])
-        items[i]["pos"]=0
-        print("item",i,"prefix",len(prefix),"suffix",len(items[i]["lrs"]))
+    apes = sorted(apes,key=lambda x:-x["cnt"])
 
-    def nis(i):
-        pos = items[i]["pos"]
-        lp = len(items[i]["prefix"])
-        llrs = len(items[i]["lrs"])
-                 
-        if lp:
-            x = items[i]["prefix"].pop(0)
-            #print("pref", items[i]["prefix"])
-            return x
-        else:
-            #print("lrs", items[i]["lrs"], "pos", items[i]["pos"],"X",items[i]["lrs"][pos])
-            x = items[i]["lrs"][pos]
-            pos+=1
-            items[i]["pos"] = pos % llrs
-
-            return x
-
-    def cnti(z,n,ac):
-        cnt=0
-        a = nis(z)
-        b = ""
-        while cnt<n:
-            cnt+=1
-            while True:
-                #print("a",a,"b",b)
-                #print("B",z,"->",a)
-                ac[int(a)]+=1
-                #print("B",ac)
-                b = nis(z)
-
-                # we do the monkeys in order. Hence, if we end up moving an item to a monkey lower than the monkey
-                # the item is located in, we have a new cycle
-                if int(b) < int(a):
-                    #print("B -tick")
-                    a=b
-                    break
-                a=b
-        return ac
-        
-    # we know the sequences of the items. Now we only have to make the apes toss them in the right order
-    # we only have 1 item in the test code
-    ac = {x:0 for x in range(len(apes))}
-
-    for i in items:
-        #print(i)
-        ac = cnti(i,1000,ac)
-        #print(ac)
-
-    
-
-#print ("B -end")
-#print("B",z,"->",a)
-#ac[int(a)]+=1
-
-print("APES 2",list(ac.values()))
+    print("Part 1:", apes[0]["cnt"]*apes[1]["cnt"])
         
