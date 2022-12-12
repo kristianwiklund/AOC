dec9030434a883465325b22b0fd7bb6c523572a4
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-12 17:00:47 +0100

    refactor

--- 2017/9/9.sh
+++ 2017/9/9.sh
@@ -4,8 +4,5 @@ echo -n "Part 1: "
 export BC_LINE_LENGTH=10000
 cat input.txt | sed 's|!.||g' | sed 's|<[^>]*>||g' | sed 's|[^{}]||g' | sed 's/{/c=c+1;/g' | sed 's/}/print " ";print c;c=c-1;/g' | sed 's/^/print "\\n";c=0;/' | tr ';' '\n'| bc -l  | sed 's/^ *//' |  sed 's/$/\n/' |  sed 's| |\n.+|g' | bc -l| tail -1
 
-echo "-module('i')." > i.erl
-echo "-export([i/0])." >> i.erl
-echo "i() ->" >> i.erl
-sed -e 's/"/\\"/g' -e 's/^/"/' -e 's/$/"./'  < input.txt >> i.erl
-erlc i.erl
+echo -n "Part 2: "
+python3 9b.py
