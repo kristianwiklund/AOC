40b6a94fc7b1ff8567f6e06fce912f68b5a11e08
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-13 18:40:49 +0100

    2017 12 a

---
+++ 2017/12/12.py
@@ -0,0 +1,16 @@
+import networkx as nx
+# usage
+import sys
+sys.path.append("../..")
+from utilities import *
+
+a = readarray("input.txt")
+G = nx.Graph()
+
+for i in a:
+    f = i[0]    
+    for t in i[2:]:
+        G.add_edge(f,t.strip(","))
+
+print("Part 1:",len(nx.node_connected_component(G,"0")))
+
