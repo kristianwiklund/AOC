abcaf86545bad76786179c9b1fc205136ec75203
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-13 07:17:39 +0100

    x

--- 2022/13/13.py
+++ 2022/13/13.py
@@ -2,59 +2,79 @@
 # sys.path.append("../..")
 # from utilities import *
 
-def comp(ls,rs):
+def comp(ls,rs, d=False):
 
-    print("-- c",ls,rs)
+    if d:
+        print("<<>> c",ls,"<<>>",rs,"<<>>")
 
     if isinstance(ls,int) and isinstance(ls,int):
+        if d:
+            print("i-i comp")
         return ls<rs
 
     try:
         l=ls.pop(0)
     except:
+        if d:
+            print("--- l short")
         # run out of left first, order good
         return True
 
     try:
         r=rs.pop(0)
     except:
+        if d:
+            print("--- r short")
         # run out of right first, order bad
         return (False)
 
     if isinstance(l,int) and isinstance(r,int):
         if l>r:
- #           print(">")
+            if d:
+                print("--- int >")
             return (False)
         if l<r:
- #           print("<")
+            if d:
+                print("--- int <")
             return True
     
     if isinstance(r,list) and isinstance(l,int):
-#        print("pa",l,ls,r,rs)
-                
         l=[l]
         l.append(ls)
         r.append(rs)
-#        print("a",l,r)
+        if d:
+            print("--- a",l,r)
         return comp(l,r)
 
     if isinstance(l,list) and isinstance(r,int):
- #       print("pa",l,ls,r,rs)
         r=[r]
         l.append(ls)
         r.append(rs)
-#        print("a",l,r)        
+        if d:
+            print("--- a",l,r)        
         return comp(l,r)
 
-    # both items are lists
     if isinstance(l,int) and isinstance(r,int):
         if l==r:
+            if d:
+                print("--- i=i retry")
             return(comp(ls,rs))
-    if comp(l,r):
-        return comp(ls,rs)
+    if d:
+        print("list,list=",isinstance(l,list),isinstance(r,list))
+        
+    if comp(l,r,d):
+        return comp(ls,rs,d)
     else:
         return False
-        
+
+assert(comp([1,1,3,1,1], [1,1,5,1,1]))
+assert(comp([[1],[2,3,4]], [[1],4]))
+assert(not comp([9],[[8,7,6]]))
+assert(comp([[4,4],4,4],[[4,4],4,4,4]))
+assert(not comp([7,7,7,7],[7,7,7]))
+assert(comp([],[3]))
+assert(not comp([[[]]],[[]]))
+assert(not comp([1,[2,[3,[4,[5,6,7]]]],8,9],[1,[2,[3,[4,[5,6,0]]]],8,9]))
 
 with open("input.debug") as fd:
 
@@ -70,8 +90,9 @@ with open("input.debug") as fd:
         r=right
 
         # chomp = i+2
+        print("================================")
         try:
-            if comp(l,r):
+            if comp(l,r, d=True):
                 score+=cnt
         except:
             print("crash")
