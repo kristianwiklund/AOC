59755ce9cb7bfef08a9241d9dea5a35737c1a548
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-16 16:07:54 +0100

    x

--- 2022/16/16.py
+++ 2022/16/16.py
@@ -2,32 +2,41 @@ import sys
 sys.path.append("../..")
 from utilities import *
 import networkx as nx
-from copy import deepcopy
+from copy import copy
 from pprint import pprint
 
-arr = readarray("input.short",split=" ")
+arr = readarray("input.txt",split=" ")
 
 G = nx.DiGraph()
 
 for i in arr:
     rate=int(i[4].split("=")[1].strip(";"))
+    if rate>0:
+        G.add_node(i[1],rate=rate)
+
+for i in arr:
 #    G.add_node(i[1],rate=rate)
     for j in i[9:]:
         j=j.strip(",")
         G.add_edge(i[1],j,weight=1)
-        if rate>0:
-            G.add_edge(i[1],i[1]+"O",capacity=rate,weight=1)
-        G.add_edge(i[1]+"O",j,weight=1)
+
+
 
 start="AA"
 print(G)
 
+# calculate the score for a series of opened valves vs the clock
 def scoreme(a, clock):
     s=0
     for i in a:
-        elapsed = clock-i[0]
+        elapsed = clock-i[0]+1
         s+=elapsed*i[1]
     return (s)
+
+
+assert(scoreme([(3,20),(6,13),(10,21),(18,22),(22,3),(25,2),],30)==1651)
+
+cache = dict()
 
 def go(G, node, clock, bleedtime, visited):
 
@@ -36,6 +45,10 @@ def go(G, node, clock, bleedtime, visited):
         print("timeout",bleedtime,visited)
         return (bleedtime, visited)
 
+    sg = G.subgraph(i)
+    if str(sg) in cache:
+        return cache[str(sg)]
+    
     children = set(nx.descendants(G, node))
     children-=set(visited)
     
@@ -56,24 +69,48 @@ def go(G, node, clock, bleedtime, visited):
             continue
 
         d = G.get_edge_data(node, child)
-        if d and "capacity" in d:
-            bleedtime.append((clock, d["capacity"], child))
-            #print(bleedtime)
-        
 
         print ("Try valve",child[0:2],"which never is visited in",visited)
         result = go(G, child, clock, bleedtime, visited+[child[0:2]])
         
         score = scoreme(result[0], clock)
-
+        
         if score>highscore:
             highscore=score
             highpath=result[1]
             highbleed=result[0]
 
+
+    if "rate" in G.nodes[node]:
+        for child in children:
+
+            if child in visited or child[0:2] in visited:
+                continue
+
+            d = G.get_edge_data(node, child)
+        
+            
+            print ("Turn on",node,"and try valve",child[0:2],"which never is visited in",visited)
+            bt = copy(bleedtime)
+            bt.append((clock+1,G.nodes[node]["rate"]))
+
+            print("BT is now",bt)
+            
+            result = go(G, child, clock+1, bt, visited+[child[0:2]])
+            
+            score = scoreme(result[0], clock)
+            
+            if score>highscore:
+                highscore=score
+                highpath=result[1]
+                highbleed=result[0]
+
     if highbleed != []:
-        print("res",highbleed)
-    return(highbleed, highpath)
+        print("res",scoreme(highbleed,clock))
+
+    cache[str(sg)]=(highbleed, highpath)
+        
+    return(highbleed, highpath, highscore)
         
 
-go(G,"AA",0,[],["AA"])
+print(go(G,"AA",0,[],["AA"]))
