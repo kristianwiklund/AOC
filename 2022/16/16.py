49fb2440287ad632b26d5362218119954510a691
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-16 19:03:15 +0100

    x

--- 2022/16/16.py
+++ 2022/16/16.py
@@ -2,18 +2,21 @@ import sys
 sys.path.append("../..")
 from utilities import *
 import networkx as nx
-from copy import copy
+from copy import deepcopy
 from pprint import pprint
 
-arr = readarray("input.txt",split=" ")
+arr = readarray("input.short",split=" ")
 
 G = nx.DiGraph()
+valves = set()
 
 for i in arr:
     rate=int(i[4].split("=")[1].strip(";"))
     if rate>0:
         G.add_node(i[1],rate=rate)
-
+        print("Valve",i[1],"added with rate",rate)
+        valves.add((i[1],rate))
+        
 for i in arr:
 #    G.add_node(i[1],rate=rate)
     for j in i[9:]:
@@ -21,96 +24,87 @@ for i in arr:
         G.add_edge(i[1],j,weight=1)
 
 
-
 start="AA"
-print(G)
-
-# calculate the score for a series of opened valves vs the clock
-def scoreme(a, clock):
-    s=0
-    for i in a:
-        elapsed = clock-i[0]+1
-        s+=elapsed*i[1]
-    return (s)
 
 
-assert(scoreme([(3,20),(6,13),(10,21),(18,22),(22,3),(25,2),],30)==1651)
+# keep moving until either the time runs out
+# or all valves are open
 
-cache = dict()
+# this calculates the relative difference between node x and node y
+def weighter(x,y):
 
-def go(G, node, clock, bleedtime, visited):
+    flowx=x[1]
+    flowy=y[1]
 
-    clock+=1
-    if clock > 30:
-        print("timeout",bleedtime,visited)
-        return (bleedtime, visited)
+    timex=x[2]
+    timey=y[2]
 
-    sg = G.subgraph(i)
-    if str(sg) in cache:
-        return cache[str(sg)]
+    now = 30-y[3]
     
-    children = set(nx.descendants(G, node))
-    children-=set(visited)
+    # we sort based on opportunity cost
+
+    if timex == timey:
+        return cmp(flowx,flowy)
+
+    rem = now
+    remx = rem-timex
+    remy = rem-timey
     
-    if children==[]:
-        print("Hit the bottom, returning",bleedtime,visited)
-        return (bleedtime, visited)
+    print (x[0],"f",flowx,"t",(timex,remx),"r",remx*flowx,"--",y[0],"f",flowy,"t",(timey,remy),"r",remy*flowy)
     
-    highscore=0
-    highpath=visited
-    highbleed=bleedtime
+    return cmp(remx*flowx,remy*flowy)
 
-    print ("== Minute",clock,"==")
-    print ("Valves:",bleedtime)
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+# ('HH', 22, 6, 0), ('CC', 2, 3, 0), ('EE', 3, 3, 0), ('JJ', 21, 3, 0), ('BB', 13, 2, 0), ('DD', 20, 2, 0)
+
+def go(G, node, opened, valves, time):
+
+    if time>=30 or len(valves)==0:
+        print("Boom",opened)
+        score=0
+        for i in opened:
+            score+=(30-i[2])*i[1]
+
+        print("score",score)
+        return
     
-    for child in children:
+    SG = deepcopy(G)
+    SG.remove_node(node)
+    
+    children = nx.descendants(G,node)
+    if len(children)==0:
+        print("The end",path)
+        return
 
-        if child in visited or child[0:2] in visited:
-            continue
+    # idea.
+    # sort the remaining valves in order of benefit - magnitude vs "lost time"
+    # (if it is too expensive to move, we have a problem)
 
-        d = G.get_edge_data(node, child)
+    distance = [(x,y,len(nx.shortest_path(G,node, x))-1,time) for x,y in valves]
 
-        print ("Try valve",child[0:2],"which never is visited in",visited)
-        result = go(G, child, clock, bleedtime, visited+[child[0:2]])
-        
-        score = scoreme(result[0], clock)
-        
-        if score>highscore:
-            highscore=score
-            highpath=result[1]
-            highbleed=result[0]
+    from functools import cmp_to_key
+    distance = sorted(distance, key=cmp_to_key(weighter),reverse=True)
+    print("goodness",distance)
+    print(valves)
 
+    # the first one in the list is the best to pick
 
-    if "rate" in G.nodes[node]:
-        for child in children:
+    path = list(nx.shortest_path(G,node,distance[0][0]))
+    path.pop(0)
+    for i in path:
+        print("== Minute",time,"==")
+        print ("You move to valve",i)
+        time+=1
+    print("== Minute",time,"==")
+    print("You open the valve")
+    opened.append((i,G.nodes[i]["rate"],time))
+    valves = deepcopy(valves)
+    valves.remove((distance[0][0],distance[0][1]))
+    print("nv",valves)
+    
+    go(G,distance[0][0],opened,valves,time+1)
 
-            if child in visited or child[0:2] in visited:
-                continue
+go(G,"AA",[],valves,1)
 
-            d = G.get_edge_data(node, child)
-        
-            
-            print ("Turn on",node,"and try valve",child[0:2],"which never is visited in",visited)
-            bt = copy(bleedtime)
-            bt.append((clock+1,G.nodes[node]["rate"]))
-
-            print("BT is now",bt)
-            
-            result = go(G, child, clock+1, bt, visited+[child[0:2]])
-            
-            score = scoreme(result[0], clock)
-            
-            if score>highscore:
-                highscore=score
-                highpath=result[1]
-                highbleed=result[0]
-
-    if highbleed != []:
-        print("res",scoreme(highbleed,clock))
-
-    cache[str(sg)]=(highbleed, highpath)
-        
-    return(highbleed, highpath, highscore)
-        
-
-print(go(G,"AA",0,[],["AA"]))
