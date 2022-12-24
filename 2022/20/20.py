2f477faac138107a98bf5ea43ee8acc5612b2668
Kristian Wiklund <githubkristian@snabela.nl>
2022-12-24 10:05:43 +0100

    2022 20 b

--- 2022/20/20.py
+++ 2022/20/20.py
@@ -6,20 +6,22 @@ from copy import deepcopy
 from pprint import pprint
 
 with open("input.txt","r") as fd:
-    arr = [int(x) for x in fd.readlines()]
+    yarr = [int(x) for x in fd.readlines()]
 
     nein = set()
-    for i in range(len(arr)):
+    iarr = deepcopy(yarr)
+    
+    for i in range(len(yarr)):
 
         c=0
         while True:
-            v = chr(c+ord("A"))+str(arr[i])
+            v = chr(c+ord("A"))+str(yarr[i])
             c+=1
 
             if not v in nein:
                 break
         nein.add(v)
-        arr[i]=v
+        iarr[i]=v
 
 
     def move(arr,what):
@@ -48,14 +50,14 @@ with open("input.txt","r") as fd:
     assert(move(["A4", "A-2", "A6", "A1", "A7", "A8", "A9"],"A-2") == ["A4", "A6", "A1", "A7", "A8", "A-2", "A9"])
 
     print("== Done running tests")
-    
+
     def shuffle(arr,barr):
         for i in barr:
             arr = move(arr,i)
             
         return arr
 
-    res = shuffle(arr,deepcopy(arr))
+    res = shuffle(iarr,deepcopy(iarr))
     i = res.index("A0")
 
     a = int(res[(i+1000)%len(res)][1:])
@@ -64,4 +66,31 @@ with open("input.txt","r") as fd:
 
     print("Part 1:",a+b+c)
 
+    iarr=deepcopy(yarr)
+    
+    nein = set()
+    for i in range(len(yarr)):
+        c=0
+        while True:
+            v = chr(c+ord("A"))+str(yarr[i]*811589153)
+            c+=1
+            if not v in nein:
+                break
+            
+        nein.add(v)
+        iarr[i]=v
+
+    barr = deepcopy(iarr)
+
+    for i in range(10):
+        iarr = shuffle(iarr,barr)
+
+    res = iarr
+    i = res.index("A0")
+
+    a = int(res[(i+1000)%len(res)][1:])
+    b = int(res[(i+2000)%len(res)][1:])
+    c = int(res[(i+3000)%len(res)][1:])
+
+    print("Part 2:",a+b+c)
     
