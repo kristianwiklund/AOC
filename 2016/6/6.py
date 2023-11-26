b988cecd7cbab4c14aefa4468d9ba1e3c609084c
Kristian Wiklund <githubkristian@snabela.nl>
2023-11-26 17:12:32 +0100

    2016 6

--- 2016/6/6.py
+++ 2016/6/6.py
@@ -6,14 +6,23 @@ from utilities import *
 from pprint import pprint
 import numpy as np
 
-def decodeme(arr):
+def decodeme(arr,modified=False):
     arr = np.matrix(arr)
     arr = arr.transpose()
-    s = "".join([list(reversed(sortdictbykey(dict((x, data.count(x)) for x in data))))[0][0] for data in arr.tolist()])
+
+    if not modified:
+        s = "".join([list(reversed(sortdictbykey(dict((x, data.count(x)) for x in data))))[0][0] for data in arr.tolist()])
+    else:
+        s = "".join([list((sortdictbykey(dict((x, data.count(x)) for x in data))))[0][0] for data in arr.tolist()])    
     return (s)
 
 arr = readarray("input.short",split="",convert=lambda x:x)
 assert(decodeme(arr)=="easter")
 
+assert(decodeme(arr,modified=True)=="advent")
+
 arr = readarray("input.long",split="",convert=lambda x:x)
-print("Password:",decodeme(arr))
+print("Password A:",decodeme(arr))
+print("Password B:",decodeme(arr,modified=True))
+
+
