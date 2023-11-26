c8a13b75df81e5312925bc7f0879e90684e6f07b
Kristian Wiklund <githubkristian@snabela.nl>
2023-11-26 21:39:43 +0100

    2016 9a

---
+++ 2016/9/9.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("../..")
+from utilities import *
+#import networkx as nx
+from copy import deepcopy
+from pprint import pprint
+#import numpy as np
+
+#arr = readarray("input.short",split="",convert=lambda x:x)
+
+
+def decompress(sx,ack=""):
+
+    S=sx.split("(",maxsplit=1)
+    if len(S)==2:
+        (s,sx)=S
+        (c,sx)=sx.split(")",maxsplit=1)
+        (cnt,rpt)=c.split("x")
+        cnt=int(cnt)
+        rpt=int(rpt)
+        ack+=s
+        it = sx[:cnt]
+        sx = sx[cnt:]
+
+        ack+=it*rpt
+        return decompress(sx,ack=ack)
+        
+    else:
+        return ack+sx
+    
+assert(decompress("ADVENT")=="ADVENT")
+assert(decompress("A(1x5)BC")=="ABBBBBC")
+assert(decompress("(3x3)XYZ")=="XYZXYZXYZ")
+assert(decompress("A(2x2)BCD(2x2)EFG")=="ABCBCDEFEFG")
+assert(decompress("X(8x2)(3x3)ABCY")=="X(3x3)ABC(3x3)ABCY")
+
+lines = [len(decompress(x)) for x in readlines("input.long")]
+print(sum(lines))
