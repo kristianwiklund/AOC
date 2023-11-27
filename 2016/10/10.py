6192b5372aced21fcc98d3c155a5caf6f31d5138
Kristian Wiklund <githubkristian@snabela.nl>
2023-11-27 23:08:54 +0100

    x

---
+++ 2016/10/10.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append("../..")
+from utilities import *
+#import networkx as nx
+from copy import deepcopy
+from pprint import pprint
+from sortedcontainers import SortedList
+from sortedcontainers import SortedDict
+from sortedcontainers import SortedSet
+#import numpy as np
+
+arr = readarray("input.short",split="",convert=lambda x:x)
+lines = readlines("input.short")
+
