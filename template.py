11403ee884d5d7683d15e3f6bc5ce6f57ba1a108
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-03 13:37:47 +0100

    updated template

--- template.py
+++ template.py
@@ -2,13 +2,14 @@ import sys
 sys.path.append("../..")
 from utilities import *
 #import networkx as nx
-from copy import deepcopy
+#from copy import deepcopy
 from pprint import pprint
-from sortedcontainers import SortedList
-from sortedcontainers import SortedDict
-from sortedcontainers import SortedSet
+#from sortedcontainers import SortedList
+#from sortedcontainers import SortedDict
+#from sortedcontainers import SortedSet
 #import numpy as np
 #import scipy
+#from functools import cache
 
 arr = readarray("input.short",split="",convert=lambda x:x)
 lines = readlines("input.short")
