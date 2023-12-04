491d853ff897e7b0316af4530cac83d3832e3c29
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-04 09:19:05 +0100

    x

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -15,11 +15,13 @@ arr = readarray("input.txt",split="|",convert=lambda x:x)
 #lines = readlines("input.short")
 
 sum=0
+yarr=[]
 for l in arr:
     win = set(ints(l[0].split(":")[1]))
     have = set(ints(l[1]))
     points = 0 if len(have&win)==0 else 2**(len(have&win)-1)
     sum+=points
-    print(win,have,points)
+#    print(win,have,points)
 
 print("Part 1:",sum)
+
