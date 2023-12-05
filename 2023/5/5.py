e88b3c169f159719e71590dd6801c851226203ac
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-05 06:47:40 +0100

    x

--- 2023/5/5.py
+++ 2023/5/5.py
@@ -11,5 +11,20 @@ from pprint import pprint
 #import scipy
 #from functools import cache
 
-arr = readarray("input.short",split="",convert=lambda x:x)
-lines = readlines("input.short")
+#arr = readarray("input.short",split="",convert=lambda x:x)
+#lines = readlines("input.short")
+
+them = dict()
+with open("input.short") as fd:
+    while fd:
+        v = readblock(fd)
+        if not v:
+            break
+        what = v[0].split(":")[0].split(" ")[0]
+        if v[0].split(":")[1] =="":
+            them[what] = v[1:]
+        else:
+            them[what] = [(v[0].split(":")[1]).strip()]+v[1:]
+
+            
+print(them)
