0892eba9dbb4523a233e7c0875228675de2bfe21
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-06 19:39:09 +0100

    2023 5b

--- 2023/5/5b.py
+++ 2023/5/5b.py
@@ -9,11 +9,19 @@ from pprint import pprint
 #from sortedcontainers import SortedSet
 #import numpy as np
 #import scipy
-from functools import cache
-from Ranges import Ranges
+#from functools import cache
+
 #arr = readarray("input.short",split="",convert=lambda x:x)
 #lines = readlines("input.short")
 
+
+def checkseed(ns, s):
+    for x in ns:
+        if s in x:
+            return True
+
+    return False
+    
 them = dict()
 with open("input.txt") as fd:
     while fd:
@@ -26,53 +34,63 @@ with open("input.txt") as fd:
         else:
             s = [(v[0].split(":")[1]).strip()]+v[1:]
 
-        them[what] = sorted([ints(x) for x in s])
+        them[what] = [ints(x) for x in s]
         
-
+print(them)
 ns=[]
 for i in range(int(len(them["seeds"][0])/2)):
     ns.append(range(them["seeds"][0][i*2],them["seeds"][0][i*2]+them["seeds"][0][i*2+1]))
 
-them["seeds"]=ns
-#print(them)
+def link(them, frm, to, n):
+    what = to+"-to-"+frm
 
-def rangify(i):
-    return (Ranges(range(i[1],i[1]+i[2])), Ranges(range(i[0],i[0]+i[2])))
+    for x in them[what]:
+        sr = x[0]
+        dr = x[1]
+        l = x[2]
+#        print(what, "n=",n, "source range=",(sr, sr+l), "dest=",dr)
+        i = range(sr, sr+l).index(n) if n in range(sr, sr+l) else -1
+        if i>-1:
+#            print (i, dr, i+dr)
+            return i+dr
 
-for x in them:
-    if x=="seeds":
-        continue
+    # any.. not mapped... same number
+    return n
 
-    v = them[x]
-    v = [rangify(y) for y in v]
-    them[x] = v
 
-def transform(them, frm, to, r):
-    t = them[frm+"-to-"+to]
-    a= []
-    for x in t:
-        # check if r matches any ranges in x
-        fr = x[0]
-        tr = x[1]
+# test data
+if len(them["seed-to-soil"])==2:
+    assert(link(them, "location", "humidity", 82)==78)
+    assert(link(them, "soil", "seed", 81)==79)
+    assert(link(them, "water", "fertilizer", 81)==81)
 
-        v = fr & tr
-        # v is the part we transform to something else
-        if len(v):
-            
-            a.append(v)
 
-                
-    a = sorted(a, key=lambda x:x.start)
-            
-    b = merge(a)
-    if r!=b[0]:
-        print (r,"!=",b, "unmerged", a)
-            
-            
-for i in them["seeds"]:
-    print("----")
-    print(i)
-    transform(them, "seed", "soil", i)
-    print("----")
+def doit(them):
 
-    
+    lowloc=None
+    maxloc = max([x[0]+x[2] for x in them["humidity-to-location"]])
+    #    print(maxloc)
+
+    print("maxloc=",maxloc)
+    import time
+    tttt = time.time()
+    for x in range(0,maxloc):
+        if not x % 1000000:
+            print (x, "time elapsed=",time.time()-tttt,"time remain=",(time.time()-tttt)*maxloc/1000000)
+#        print ("seed", i)
+        a = link(them, "location","humidity",x)
+        b = link(them, "humidity", "temperature", a)
+        c = link(them, "temperature", "light", b)
+        d = link(them, "light", "water", c)
+        e = link(them, "water", "fertilizer", d)
+        f = link(them, "fertilizer", "soil", e)
+        g = link(them, "soil", "seed", f)
+
+        if checkseed(ns, g):
+            print(x,g)
+            break
+
+    print ("lowloc:",x)
+
+doit(them)
+
