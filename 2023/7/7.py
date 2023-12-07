43e3b2c4013009b75692ef68450cd5e91f0b19da
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-07 13:58:04 +0100

    2023 7a

--- 2023/7/7.py
+++ 2023/7/7.py
@@ -10,14 +10,70 @@ from pprint import pprint
 import numpy as np
 #import scipy
 #from functools import cache
+from functools import cmp_to_key
 
-arr = readarray("input.short",split=" ",convert=lambda x:x)
+arr = readarray("input",split=" ",convert=lambda x:x)
 #lines = readlines("input.short")
 
-def rank(s):
-    unique_values, counts = np.unique(s, return_counts=True)
+def hand(c):
 
-    print (unique_values, counts)
+    c = sorted(c,reverse=True)
 
-v = [rank(list(x[0])) for x in arr]
-print(v)
+    if c[0]==5:
+        return 7
+    if c[0]==4:
+        return 6
+    if c[0]==3 and c[1]==2:
+        return 5
+    if c[0]==3:
+        return 4
+    if c[0]==2 and c[1]==2:
+        return 3
+    if c[0]==2:
+        return 2
+
+    return 1
+
+T = {'A':14,'K':13,'Q':12,'J':11,'T':10}
+
+def rank(s1,s2):
+    s1 = list(s1)
+    s2 = list(s2)
+    unique_values1, counts1 = np.unique(s1, return_counts=True)
+    unique_values2, counts2 = np.unique(s2, return_counts=True)
+
+    h1 = hand(counts1)
+    h2 = hand(counts2)
+
+    if h1!=h2:
+        return h1-h2
+
+    s1 = np.array([int(x) if x.isdigit() else T[x] for x in s1])
+    s2 = np.array([int(x) if x.isdigit() else T[x] for x in s2])
+    t = s1-s2
+
+    for x in t:
+        if x==0:
+            continue
+        return x
+    return 0
+    
+
+v = [x[0] for x in arr]
+v = sorted(v,key=cmp_to_key(rank))
+
+hands= {}
+
+
+for x in arr:
+    print(x)
+    hands[x[0]]=int(x[1])
+
+print(hands)
+
+s=0
+for i in range(len(v)):
+    print(i+1,v[i],hands[v[i]])
+    s+=(i+1)*hands[v[i]]
+
+print("Part 1:",s)
