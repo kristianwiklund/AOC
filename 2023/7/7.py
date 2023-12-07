f0bf01eef45b7c27f2709ed94f0beb981476d639
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-07 13:34:50 +0100

    x

--- 2023/7/7.py
+++ 2023/7/7.py
@@ -7,9 +7,17 @@ from pprint import pprint
 #from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
 #from sortedcontainers import SortedSet
-#import numpy as np
+import numpy as np
 #import scipy
 #from functools import cache
 
-arr = readarray("input.short",split="",convert=lambda x:x)
-lines = readlines("input.short")
+arr = readarray("input.short",split=" ",convert=lambda x:x)
+#lines = readlines("input.short")
+
+def rank(s):
+    unique_values, counts = np.unique(s, return_counts=True)
+
+    print (unique_values, counts)
+
+v = [rank(list(x[0])) for x in arr]
+print(v)
