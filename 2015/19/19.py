479e9b64ce0a1a2ae985f9e2954812c418590bda
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-10 11:12:03 +0100

    2023 10a

--- 2015/19/19.py
+++ 2015/19/19.py
@@ -1,24 +1,54 @@
 #!/usr/bin/python3
 
-from input import input
-from input import tokens
+from inputshort import input
+from inputshort import tokens
 import re
 import networkx as nx
+from functools import cache
 
 G = nx.DiGraph()
 
-sum=0
-bop = list()
-s = input()
-t = tokens()
 
-all = set()
+def subit(s):
+    bop = list()
+    t = tokens()
 
-for i in tokens():
-    r = t[i]
+    all = set()
 
-    for rr in r:
-        all|=set([s[:m.start()] + rr + s[m.end():] for m in re.finditer(i,s)])
+    for i in tokens():
+        r = t[i]
 
+        for rr in r:
+            all|=set([s[:m.start()] + rr + s[m.end():] for m in re.finditer(i,s)])
+
+    return all
+
+all = subit(input())
+            
 print(all)
 print("Answer to 1: ",len(all))
+
+m = "e"
+
+@cache
+def doit(m, target, depth=0):
+    global tokens
+    
+    all = subit(m)
+
+    mm = []
+    
+    for i in all:
+        print(i)
+        if i==target:
+            return depth+1
+        else:
+            mm.append[doit(i, target, depth+1)]
+
+        
+    return min(mm)
+
+print(doit("e","HOHOHO",0))
+        
+    
+    
---
+++ 2023/10/10.py
@@ -0,0 +1,100 @@
+import sys
+sys.path.append("../..")
+from utilities import *
+#import networkx as nx
+#from copy import deepcopy
+from pprint import pprint
+#from sortedcontainers import SortedList
+#from sortedcontainers import SortedDict
+#from sortedcontainers import SortedSet
+import numpy as np
+#import scipy
+#from functools import cache
+
+arr = readarray("input",split="",convert=lambda x:x)
+#lines = readlines("input.short")
+
+#pprint(arr)
+
+
+
+orr = ["|7F","-J7","|LJ","-LF"]
+od = [(0,-1),(1,0),(0,1),(-1,0)]
+
+def findstart(arr,x,y):
+    global orr, od
+    
+    assert(arr[y][x]=='S')
+
+    for i in range(len(od)):
+        (dx,dy) = od[i]
+        pp = orr[i]
+        if checkpos(arr,x+dx, y+dy, lambda v:v in pp, outofbounds=False):
+            return i
+
+
+[(x,y)]=[(x,y) for x in range(len(arr[0])) for y in range(len(arr)) if arr[y][x]=='S']
+        
+d =  findstart(arr,x,y)
+#print("Initial: ", d, x,y)
+
+def findmove(arr, d, x, y):
+    global orr, dd
+
+    me = arr[y][x]
+
+    # this can be done with an array
+    match me:
+        case "|":
+            d = d # continue straight ahead
+        case "-":
+            d = d # continue straight ahead
+        case "L":
+            if d==2:
+                d=1
+            else:
+                d=0
+        case "F":
+            if d==0:
+                d=1
+            else:
+                d=2
+        case "J":
+            if d==1:
+                d=0
+            else:
+                d=3
+        case "7":
+            if d==0:
+                d=3
+            else:
+                d=2
+    return d
+
+
+ta=[['-', 'L', '|', 'F', '7'], ['7', 'S', '-', '7', '|'], ['L', '|', '7', '|', '|'], ['-', 'L', '-', 'J', '|'], ['L', '|', '-', 'J', 'F']]
+
+assert(findmove(ta, 2, 1, 3)==1)
+assert(findmove(ta, 3, 1, 3)==0)
+
+def move(arr, d, x, y):
+    global orr, od
+
+    (dx,dy)=od[d]
+    x+=dx
+    y+=dy
+    d=findmove(arr,d,x,y)
+
+    return (d,x,y)
+
+
+p=[(x,y)]
+
+while len(p)==1 or arr[y][x]!='S':
+    (d,x,y) = move(arr,d,x,y)
+    p.append((x,y))
+
+#print(p)
+
+#printpath(p, background=arr)
+print("Part 1:",int(len(p)/2)    )
