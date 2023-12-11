a503d1a1592c980c50fa057e79574a58b462150a
Kristian Wiklund <githubkristian@snabela.nl>
2023-12-11 15:41:50 +0100

    2023 11

--- 2023/11/11b.py
+++ 2023/11/11b.py
@@ -6,7 +6,7 @@ from copy import copy
 from pprint import pprint
 #from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
-#from sortedcontainers import SortedSet
+from sortedcontainers import SortedSet
 import numpy as np
 import scipy.ndimage
 
@@ -16,67 +16,43 @@ import scipy.ndimage
 arr = readarray("input",split="",convert=lambda x:1 if x=='#' else 0)
 #lines = readlines("input.short")
 
-a =np.array(arr)
-print(a)
-print("---")
-labels,  numl = scipy.ndimage.label(a, structure=[[0,0,0],[0,1,0],[0,0,0]])
-
-#p=[]
-#for v in labels:
-#    if sum(v)==0:
-#        p.append(v)
-#    p.append(v)
-
-#p=np.array(p)
-#a=np.transpose(p)
-
-#p=[]
-#for v in a:
-#    if sum(v)==0:
-#        p.append(v)
-#    p.append(v)
-
-p=arr
-a = np.array(p)
-#print(p)
+a = arr
 
 pts = [(x,y) for x in range(len(a[0])) for y in range(len(a)) if a[y][x]]
-#print(pts)
 
-cols=[]
-for c in set([x[0] for x in pts]):
-    cols.append(c)
+cols = list(SortedSet([x[0] for x in pts]))
+rows = list(SortedSet([x[1] for x in pts]))
 
-rows=[]
-for r in set([x[1] for x in pts]):
-    rows.append(r)
+print(cols,rows)
+ocols=[0]+[cols[x+1]-cols[x]-1 for x in range(len(cols)-1)]
+orows=[0]+[rows[x+1]-rows[x]-1 for x in range(len(rows)-1)]
 
-#print(cols, rows)
-ocols=copy(cols)
-orows=copy(rows)
-for i in range(len(cols)-1,-1,-1):
-    d = cols[i]-cols[i-1]
-    if d>1:
-        cols[i]+=1000000*d
-        
+tc = {cols[i]:ocols[i] for i in range(len(cols)) if ocols[i]}
+tr = {rows[i]:orows[i] for i in range(len(cols)) if orows[i]}
 
-for i in range(len(rows)-1,-1,-1):
-    d = rows[i]-rows[i-1]
-    if d>1:
-        rows[i]+=1000000*d        
+def boffe(tx, p, mf):
+    
+    v = sum([tx[x]*mf for x in tx if x<=p])
+    return v
 
-#print(cols, rows)
-
-trows = {orows[i]:rows[i] for i in range(len(rows))}
-tcols = {ocols[i]:cols[i] for i in range(len(cols))}
-
-print(trows, tcols)
-
+print(tc,tr)
+print(boffe(tc,6,1))
 
 s=0
+mf=1000000-1
 for j in range(len(pts)-1):
     for i in range(j,len(pts)):
-        print(pts[j],pts[i])
-        s+=abs(tcols[pts[j][0]]-tcols[pts[i][0]])+abs(trows[pts[j][1]]-trows[pts[i][1]])
+        (x1, y1) = pts[j]
+        (x2, y2) = pts[i]
+
+        x1+=boffe(tc,x1,mf)
+        x2+=boffe(tc,x2,mf)
+
+        y1+=boffe(tr,y1,mf)
+        y2+=boffe(tr,y2,mf)
+
+        s+=abs(x1-x2)+abs(y1-y2)
+        
+        
 
 print(s)
