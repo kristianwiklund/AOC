7fa5b8ddc7635d1ff1d08649784158784e6b93b1
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-15 08:16:20 +0100

    2024 15

--- 2024/15/15b.py
+++ 2024/15/15b.py
@@ -15,7 +15,7 @@ from pprint import pprint
 #arr = readarray("input.short",split="",convert=lambda x:x)
 #lines = readlines("input.short")
 
-with open("input.short.2","r") as fd:
+with open("input","r") as fd:
 
     arr = readblock(fd, convert=lambda x:[x for x in x])
     b = readblock(fd, convert=lambda x:[x for x in x])
@@ -92,12 +92,12 @@ with open("input.short.2","r") as fd:
         if (xx,yy) in w["wall"]:
             return
 
-        print("can move", lb)
+#        print("can move", lb)
 
         # push the boxes one step in direction d starting with the box furthest away
 
         for i in lb[::-1]:
-            print("push",i,"one step in",d,"-->",end="")
+#            print("push",i,"one step in",d,"-->",end="")
             x,y=i
             t = w["box"][(x,y)]
             del w["box"][(x,y)]
@@ -106,7 +106,7 @@ with open("input.short.2","r") as fd:
             a=(t[0][0]+dirs[d][0], t[0][1]+dirs[d][1])
             b=(t[1][0]+dirs[d][0], t[1][1]+dirs[d][1])
 
-            print((x,y),":",(a,b))
+ #           print((x,y),":",(a,b))
             w["box"][(x,y)]=(a,b)
             
         w["me"]=(ox,oy)
@@ -114,7 +114,7 @@ with open("input.short.2","r") as fd:
     # recursive box finder
     def findlbv(w,p,d,lb):
 
-        print("findlbv checking",p,"in dir",d)
+#        print("findlbv checking",p,"in dir",d)
         xx,yy=p
         xx+=dirs[d][0]
         yy+=dirs[d][1]
@@ -162,17 +162,17 @@ with open("input.short.2","r") as fd:
         # the trivial case is that nothing is in front of us - we have already checked for that and for a wall,
         # so now we need to find all the boxes the box might move and move them. done the same way as in smolpotat
 
-        print(list(w["box"][(xx,yy)]))
+      #  print(list(w["box"][(xx,yy)]))
         lb = list(set(list(w["box"][(xx,yy)])+findlbv(w, w["box"][(xx, yy)][0], d, [])+findlbv(w, w["box"][(xx, yy)][1], d, [])))
-        print(lb)
+      #  print(lb)
 
         # lb contains all boxes that potentially move when we move. now figure out if we can move or not
         # start by sorting the boxes in "furthest away from dude first" order
         dd = [abs(w["me"][1]-t[1]) for t in lb]
-        print(dd)
+#        print(dd)
 
         lb = [x for _, x in sorted(zip(dd, lb))][::-1]
-        print(lb)
+#        print(lb)
 
         for x in lb:
             # if any of the boxes in the cone have a wall next to them in the movement dir, we cannot move
@@ -182,7 +182,7 @@ with open("input.short.2","r") as fd:
         # yay move.
 
         for i in lb:
-            print("push",i,"one step in",d,"-->",end="")
+ #           print("push",i,"one step in",d,"-->",end="")
             x,y=i
             t = w["box"][(x,y)]
             del w["box"][(x,y)]
@@ -191,24 +191,41 @@ with open("input.short.2","r") as fd:
             a=(t[0][0]+dirs[d][0], t[0][1]+dirs[d][1])
             b=(t[1][0]+dirs[d][0], t[1][1]+dirs[d][1])
 
-            print((x,y),":",(a,b))
+  #          print((x,y),":",(a,b))
             w["box"][(x,y)]=(a,b)
             
         w["me"]=(xx,yy)
         
         
         
-
+    t = {'^':0,'>':1,'v':2,'<':3}
     world = dooz(arr)
-    printworld(world)
+    #printworld(world)
     
-    storpotat(world, 3)
-    printworld(world)
+    #storpotat(world, 3)
+    #printworld(world)
 
-    world["me"]=(7,5)
-    printworld(world)
+    #world["me"]=(7,5)
+    #printworld(world)
 
-    storpotat(world,0)
-    printworld(world)
+    #storpotat(world,0)
+    #printworld(world)
+
+    def gps(world):
+
+        s = set()
+        for i in world["box"]:
+            s.add(world["box"][i][0])
+#        print(s)
+
+        return sum([i[1]*100+i[0] for i in s])
     
+    for dd in b:
+        d=t[dd]
+        
+        storpotat(world, d)
+        print(".",end="")
+ #       printworld(world)
+
+    print(gps(world))
 
