e1e9e4dffb24f5c297dcf9fd9e480593163b66d7
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-17 09:56:42 +0100

    x

--- 2024/17/17.py
+++ 2024/17/17.py
@@ -3,7 +3,7 @@ sys.path.append("../..")
 from utilities import *
 #import networkx as nx
 #import matplotlib.pyplot as plt
-#from copy import deepcopy
+from copy import deepcopy
 from pprint import pprint
 #from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
@@ -13,7 +13,7 @@ from pprint import pprint
 #from functools import cache
 
 #arr = readarray("input.short",split="",convert=lambda x:x)
-lines = readlines("input")
+lines = readlines("input.short")
 
 ABC = [ints(lines[0])[0], ints(lines[1])[0], ints(lines[2])[0]]
 P = ints(lines[4])
@@ -21,7 +21,6 @@ P = ints(lines[4])
 print(ABC)
 print(P)
 
-IP = 0
 
 def combo(o):
     global ABC
@@ -30,45 +29,59 @@ def combo(o):
         return ABC[o-4]
     
     return o
-        
+
+def run(ABC,P):
+#    global ABC
+    IP = 0
+    
+    output=[]
+    
+    while True:
+
+        if IP>=len(P):
+            return output
+    
+        i = P[IP]
+        IP+=1
+        o = P[IP]
+        IP+=1
+
+    
+        match i:
+
+            case 0:
+                ABC[0] = ABC[0] // (2**combo(o))
+            case 1:
+                ABC[1] = ABC[1] ^ o
+            case 2:
+                ABC[1] = combo(o) % 8
+            case 3:
+                if ABC[0]:
+                    IP = o
+            case 4:
+                ABC[1] = ABC[1] ^ ABC[2]
+            case 5:
+                output.append(combo(o)%8)
+            case 6:
+                ABC[1] = ABC[0] // (2**combo(o))
+            case 7:
+                ABC[2] = ABC[0] // (2**combo(o))
+
+    
+PQI = ABC
+
+q = 0
 
 while True:
-
-    if IP>=len(P):
+    q=q+1
+#    print(q)
+    ABC = deepcopy(PQI)
+    ABC[0]=q
+    r = run(ABC,P)
+    if r==P:
+        print ("match", q)
+        print (r,P)
         break
-    
-    i = P[IP]
-    IP+=1
-    o = P[IP]
-    IP+=1
-
-#    print(IP-2,":",i,o)
-    
-    match i:
-
-        case 0:
-#            print("ADV")
-            ABC[0] = ABC[0] // (2**combo(o))
-#            print("ADV /",2**combo(o),"->",ABC[0])
-        case 1:
-            ABC[1] = ABC[1] ^ o
-        case 2:
-#            print("BST ->",ABC[1])
-            ABC[1] = combo(o) % 8
-        case 3:
-#            print("JNZ ?",ABC[0],"->",o)
-            if ABC[0]:
-                IP = o
-        case 4:
-            ABC[1] = ABC[1] ^ ABC[2]
-        case 5:
-#            print("OUT",combo(o),o, "->",end="");
-            print(combo(o)%8,end=",")
-#            print("")
-        case 6:
-            ABC[1] = ABC[0] // (2**combo(o))
-        case 7:
-            ABC[2] = ABC[0] // (2**combo(o))
-
-    
-    
+#    print(q,r,P)
+    if not q%100000:
+        print(q)
