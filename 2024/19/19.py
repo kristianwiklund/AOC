e899020e2d09eb31906bbdc5f4c0e6b2fa266aa7
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-20 22:55:28 +0100

    2024 19 a

--- 2024/19/19.py
+++ 2024/19/19.py
@@ -5,80 +5,55 @@ from utilities import *
 #import matplotlib.pyplot as plt
 #from copy import deepcopy
 from pprint import pprint
-#from sortedcontainers import SortedList
+from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
 #from sortedcontainers import SortedSet
 #import numpy as np
 #import scipy
 #from functools import cache
-from cachetools import *
 
 #arr = readarray("input.short",split="",convert=lambda x:x)
 lines = readlines("input")
-#from multiprocessing import Pool
-
-pat = sorted(lines[0].replace(" ","").split(","), key=len)
-#print(pat)
-lines = lines[2:]
-#print(lines)
+pat = sorted(lines[0].replace(" ","").split(","),key=lambda x:-len(x))
+lines=lines[2:]
 
 @cache
-def match(line):
-    global pat
-    fr=[]
-    for i in pat:
-        if line.startswith(i):
-            fr.append((i, line[len(i):]))
-
-    return fr
-
-@cached(cache={}, key=lambda pile:pile[1])
-def make(pile):
-    global pat
-    o,c,l = pile
-
-    v = match(c)
-    if not len(v):
-        return None
-
-    newp = [(o,y,l+[x]) for x,y in v if len(y)]
-    nawp = [(o,y,l+[x]) for x,y in v if not len(y)]
-    return (newp, nawp)
-
-# idea:
-# move along the track. find spots that have a manhattan distance of <=20 and check if those are connectable
-# without touching other track parts along the way
-
-
-
-   
+def boll(s):
     
-@cache
-def makemake(l):
-
-    X=[(l,l,[])]
-    while len(X):
-       x = X.pop()
-       t = make(x)
-       # returns: a list of (original name, remaining text, match list)
-       if t:
-           if t[1]:
-               return True
-           X+=t[0]
+    global pat
+    hyst=set()
+    #    koff = SortedList(key=len)
+    #    koff.add(s)
+    koff = [s]
     
+    while len(koff):
+#        koff=sorted(koff,key=lambda x:len(x)) 
+#        print(koff)
+                
+        s = koff.pop()
+        if s in hyst:
+            continue
+        hyst.add(s)
+        
+        x = [i for i,p in enumerate(pat) if s.startswith(p)]
+        if not len(x):
+            print("discard",s)
+            continue
        
-    return False
+        for i in x:
+            v = s[len(pat[i]):]
+            if not v:
+                return 1
+            koff.append(v)
 
-    
-s=set()
+    return 0
+
 c=0
-a=0
-for l in lines:
-    v = makemake(l)
-    if v:
-        print(l)
-        a+=1
-    c+=1
-    print(a,"/",c,"ok")
+cc=0
+for i in lines:
+    print(cc,c)
+    cc+=1
 
-print("track length",len(p))
+    c+=boll(i)
+       
+print(c)
