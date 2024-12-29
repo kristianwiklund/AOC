4028d8fb0b94ea81dbe563c456f3c0236cd6489e
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-29 13:11:01 +0100

    2022 19 a

--- 2022/19/19.py
+++ 2022/19/19.py
@@ -11,10 +11,10 @@ from sortedcontainers import SortedSet
 #import numpy as np
 #import scipy
 from functools import cache
-from cachetools import cached
+from cachetools import cached,LRUCache
 from cachetools.keys import hashkey
 
-arr = readarray("input.short",split=".",convert=lambda x:x)
+arr = readarray("input.txt",split=".",convert=lambda x:x)
 
 bp={}
 
@@ -29,72 +29,102 @@ for ii,i in enumerate(arr):
     print(bp)
 
 best=0
+firstgeode=1000
 
-front=SortedSet(key=lambda x:-x[3]*(x[2][1]*2+x[2][2]*4+x[2][3]*8))
+front=SortedSet(key=lambda x:(x[2][0]+x[2][1]*10+x[2][2]*100+x[2][3]*1000))
 
 def mkstr(t,inv,bots,buy):
-#    return ""
-    return str(t)+":"+str(inv)+" "+str(bots)+"\n"
+    return ""
+    return str(t)+":"+str(inv)+" "+str(bots)+buy+"\n"
     
 
+prune=0
+
+#@cached(cache=LRUCache(maxsize=10000000),key=lambda bp,bots,inv,t,tl:hashkey(bp,bots,inv,t))
 @cached(cache={},key=lambda bp,bots,inv,t,tl:hashkey(bp,bots,inv,t))
 def diggy(bp, bots, inv, t, tl):
     global best
+    global firstgeode
+    global prune
     
     ore, clay, obsidian, geode = 0,1,2,3
 
     bots=list(bots)
     inv=list(inv)
 
+#    print(t,inv,bots)
+    
     # mining
-    for i in range(4):
-        inv[i]+=bots[i]
+#¤    for i in range(4):
+#        inv[i]+=bots[i]
 
     # check if we are done
     if t>=24:
-        if inv[geode]>best:
-            print(inv[geode],tl)
-            best=inv[geode]
-            return inv[geode]
+        if inv[geode]+bots[geode]>best:
+            print("NB:",inv[geode]+bots[geode],"\ntl:",tl)
+            print("------------------------")
+            best=inv[geode]+bots[geode]
+            return best
         else:
-            return False
+            return 0
 
-
-#    print(inv)
-    
-
+    if bots[geode]==0 and firstgeode<t:
+        prune+=1
+        return 0
+        
+    om = 0
 
     if inv[ore]>=bp[geode][0] and inv[obsidian]>=bp[geode][1]:
         ap = (bots[0],bots[1],bots[2],bots[3]+1)
-        front.add((bp, ap, (inv[ore]-bp[geode][0], inv[clay], inv[obsidian]-bp[geode][1], inv[geode]), t+1,tl+mkstr(t,inv,bots,"geode")))
-#        print("bpt")
+
+        if t+1<firstgeode:
+            print("first geode at",t+1,"old=",firstgeode)
+            firstgeode=t+1
+            
+        nm=diggy(bp, ap, (inv[ore]-bp[geode][0]+bots[ore], inv[clay]+bots[clay], inv[obsidian]-bp[geode][1]+bots[obsidian], inv[geode]+bots[geode]), t+1,tl+mkstr(t,inv,bots,"geode"))
+        #        print("bpt")
+        om = max(nm,om)
         
     if inv[ore]>=bp[obsidian][0] and inv[clay]>=bp[obsidian][1]:
         ap = (bots[0],bots[1],bots[2]+1,bots[3])
-        front.add((bp, ap, (inv[ore]-bp[obsidian][0],inv[clay]-bp[obsidian][1],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"obs")))
+        nm=diggy(bp, ap, (inv[ore]+bots[ore]-bp[obsidian][0],inv[clay]+bots[clay]-bp[obsidian][1],inv[obsidian]+bots[obsidian],inv[geode]+bots[geode]),t+1,tl+mkstr(t,inv,bots,"obs"))
         #        print("bpt2")
-
+        om = max(nm,om)
+        
     if inv[ore]>=bp[clay]:
         ap = (bots[0],bots[1]+1,bots[2],bots[3])
-        front.add((bp, ap, (inv[ore]-bp[clay],inv[clay],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"clay")))
-#       print("bpt3")
-        
+        nm=diggy(bp, ap, (inv[ore]+bots[ore]-bp[clay],inv[clay]+bots[clay],inv[obsidian]+bots[obsidian],inv[geode]+bots[geode]),t+1,tl+mkstr(t,inv,bots,"clay"))
+        #       print("bpt3")
+        om = max(nm,om)
+                
     if inv[ore]>=bp[ore]:
+#        if t==3:
+#            print(inv[ore],bp[ore])
+#            sys.exit()
         ap = (bots[0]+1,bots[1],bots[2],bots[3])
-        front.add((bp, ap, (inv[ore]-bp[ore],inv[clay],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"ore")))
-#        print("bpt4")
+        nm=diggy(bp, ap, (inv[ore]+bots[ore]-bp[ore],inv[clay]+bots[clay],inv[obsidian]+bots[obsidian],inv[geode]+bots[geode]),t+1,tl+mkstr(t,inv,bots,"ore"))
+        #        print("bpt4")
+        om = max(nm,om)
         
-    front.add((bp, tuple(bots), tuple(inv),t+1,tl+mkstr(t,inv,bots,"")))
-#    print("bpt5")
-                  
-front.add((bp[1],(1,0,0,0),(0,0,0,0),1,""))
+    nm=diggy(bp, tuple(bots), (inv[ore]+bots[ore],inv[clay]+bots[clay],inv[obsidian]+bots[obsidian],inv[geode]+bots[geode]),t+1,tl+mkstr(t,inv,bots,""))
+    #    print("bpt5")
+    om = max(nm,om)
 
+    return om
+    
+#front.add((bp[1],(1,0,0,0),(0,0,0,0),1,""))
 
-while len(front):
-    v = front.pop()
-    (bp,bots,inv,t,tl) = v
-    diggy(bp,bots,inv,t,tl)
-    if not len(front) % 10000:
-        print(best,len(front))
-print(best)
+ql=0
+for x in bp.keys():
+    print("Running",x)
+    best=0
+    prune=0
+    firstgeode=23298039823
+    diggy.cache.clear()
+    
+    a=diggy(bp[x],(1,0,0,0),(0,0,0,0),1,"")
+    ql+=(1+x)*a
+    print(x,"got",a,"pruned",prune)
 
+print(ql)
+assert(ql>969)
