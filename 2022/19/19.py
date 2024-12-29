acfccdbb500ee4bae3d5a946a4de998779d7f6fd
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-29 10:49:29 +0100

    x

--- 2022/19/19.py
+++ 2022/19/19.py
@@ -7,10 +7,12 @@ from utilities import *
 from pprint import pprint
 #from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
-#from sortedcontainers import SortedSet
+from sortedcontainers import SortedSet
 #import numpy as np
 #import scipy
 from functools import cache
+from cachetools import cached
+from cachetools.keys import hashkey
 
 arr = readarray("input.short",split=".",convert=lambda x:x)
 
@@ -28,95 +30,73 @@ for ii,i in enumerate(arr):
 
 best=0
 
-@cache
-def run(bp, bots, inv, building, t, tl):
+front=SortedSet(key=lambda x:-x[4]*(x[2][1]*2+x[2][2]*4+x[2][3]*8))
+
+def mkstr(t,inv,bots,buy):
+#    return ""
+    return str(t)+":"+str(inv)+" "+str(bots)+"\n"
+    
+
+@cached(cache={},key=lambda bp,bots,inv,building,t,tl:hashkey(bp,bots,inv,t))
+def diggy(bp, bots, inv, building, t, tl):
     global best
     
     ore, clay, obsidian, geode = 0,1,2,3
 
     bots=list(bots)
     inv=list(inv)
-    tl=list(tl)
-    
-    if t>24:
-#        print(inv[geode])
-#        print(bots,inv)
-#        print(inv[geode],tl)
-        if inv[geode]>best:
-            print("New best:", inv[geode])
-            best=inv[geode]
-        return (inv[geode], bots, tl)
 
+    # mining
     for i in range(4):
         inv[i]+=bots[i]
 
-    if building:
-        bots[building]+=1
-        tl.append((t,building))
+    # check if we are done
+    if t>=24:
+        if inv[geode]>best:
+            print(inv[geode],tl)
+            best=inv[geode]
+            return inv[geode]
+        else:
+            return False
+
 
         
-    print(t,": digging, now we have",inv,"ores and",bots,"bots")
-        
-
-        
-    # try buying things
-
-    mx = 0
-    nx = None
-    nt = ""
-    b=None
     
-    tl=tuple(tl)
+
+#    print(inv)
+    
+
 
     if inv[ore]>=bp[geode][0] and inv[obsidian]>=bp[geode][1]:
-        #print("buy geode")
-        px,bx,tx = run(bp, tuple(bots), (inv[ore]-bp[geode][0], inv[clay], inv[obsidian]-bp[geode][1], inv[geode]), geode, t+1,tl)
-        if px>mx:
-            mx=px
-            nx=bx
-            nt=tx
-            b=geode
-    if inv[ore]>=bp[obsidian][0] and inv[clay]>=bp[obsidian][1]:
-        px,bx,tx = run(bp, tuple(bots), (inv[ore]-bp[obsidian][0],inv[clay]-bp[obsidian][1],inv[obsidian],inv[geode]),obsidian,t+1,tl)
-        #print("buy obsidian")
-        if px>mx:
-            mx=px
-            nx=bx
-            nt=tx
-            b=obsidian
-    if inv[ore]>=bp[clay]:
-        #print("buy clay")        
-        px,bx,tx = run(bp, tuple(bots), (inv[ore]-bp[clay],inv[clay],inv[obsidian],inv[geode]),clay,t+1,tl)
-        if px>mx:
-            mx=px
-            nx=bx
-            nt=tx
-            b=clay
-
-    if inv[ore]>=bp[ore]:
-        #print("buy ore")
-        px,bx,tx = run(bp, tuple(bots), (inv[ore]-bp[ore],inv[clay],inv[obsidian],inv[geode]),ore,t+1,tl)
-        if px>mx:
-            mx=px
-            nx=bx
-            nt=tx
-            b=ore
-            
-
-    #else:      
-    px,bx,tx = run(bp, tuple(bots), tuple(inv),False,t+1,tl)
-    if px>mx:
-        mx=px
-        nx=bx
-        nt=tx
-        b=None
-
-#    if b and mx==best:
-#        print(t+1,"bought",b,"inv=",inv)
+        ap = (bots[0],bots[1],bots[2],bots[3]+1)
+        front.add((bp, ap, (inv[ore]-bp[geode][0], inv[clay], inv[obsidian]-bp[geode][1], inv[geode]), geode, t+1,tl+mkstr(t,inv,bots,"geode")))
+#        print("bpt")
         
-    return (mx,nx,nt)
+    if inv[ore]>=bp[obsidian][0] and inv[clay]>=bp[obsidian][1]:
+        ap = (bots[0],bots[1],bots[2]+1,bots[3])
+        front.add((bp, ap, (inv[ore]-bp[obsidian][0],inv[clay]-bp[obsidian][1],inv[obsidian],inv[geode]),obsidian,t+1,tl+mkstr(t,inv,bots,"obs")))
+        #        print("bpt2")
+
+    if inv[ore]>=bp[clay]:
+        ap = (bots[0],bots[1]+1,bots[2],bots[3])
+        front.add((bp, ap, (inv[ore]-bp[clay],inv[clay],inv[obsidian],inv[geode]),clay,t+1,tl+mkstr(t,inv,bots,"clay")))
+#       print("bpt3")
+        
+    if inv[ore]>=bp[ore]:
+        ap = (bots[0]+1,bots[1],bots[2],bots[3])
+        front.add((bp, ap, (inv[ore]-bp[ore],inv[clay],inv[obsidian],inv[geode]),ore,t+1,tl+mkstr(t,inv,bots,"ore")))
+#        print("bpt4")
+        
+    front.add((bp, tuple(bots), tuple(inv),False,t+1,tl+mkstr(t,inv,bots,"")))
+#    print("bpt5")
+                  
+front.add((bp[1],(1,0,0,0),(0,0,0,0),False,1,""))
 
 
-for i in bp:
-    best=0
-    print (i,":",run(bp[i],(1,0,0,0),(0,0,0,0),False,1,()))
+while len(front):
+    v = front.pop()
+    (bp,bots,inv,building,t,tl) = v
+    diggy(bp,bots,inv,building,t,tl)
+    print(best,len(front))
+print(best)
+
