b146cf8a8729a69d7454226d787a43006386cfc2
Kristian Wiklund <githubkristian@snabela.nl>
2024-12-29 10:54:23 +0100

    x

--- 2022/19/19.py
+++ 2022/19/19.py
@@ -30,15 +30,15 @@ for ii,i in enumerate(arr):
 
 best=0
 
-front=SortedSet(key=lambda x:-x[4]*(x[2][1]*2+x[2][2]*4+x[2][3]*8))
+front=SortedSet(key=lambda x:-x[3]*(x[2][1]*2+x[2][2]*4+x[2][3]*8))
 
 def mkstr(t,inv,bots,buy):
 #    return ""
     return str(t)+":"+str(inv)+" "+str(bots)+"\n"
     
 
-@cached(cache={},key=lambda bp,bots,inv,building,t,tl:hashkey(bp,bots,inv,t))
-def diggy(bp, bots, inv, building, t, tl):
+@cached(cache={},key=lambda bp,bots,inv,t,tl:hashkey(bp,bots,inv,t))
+def diggy(bp, bots, inv, t, tl):
     global best
     
     ore, clay, obsidian, geode = 0,1,2,3
@@ -60,43 +60,41 @@ def diggy(bp, bots, inv, building, t, tl):
             return False
 
 
-        
-    
-
 #    print(inv)
     
 
 
     if inv[ore]>=bp[geode][0] and inv[obsidian]>=bp[geode][1]:
         ap = (bots[0],bots[1],bots[2],bots[3]+1)
-        front.add((bp, ap, (inv[ore]-bp[geode][0], inv[clay], inv[obsidian]-bp[geode][1], inv[geode]), geode, t+1,tl+mkstr(t,inv,bots,"geode")))
+        front.add((bp, ap, (inv[ore]-bp[geode][0], inv[clay], inv[obsidian]-bp[geode][1], inv[geode]), t+1,tl+mkstr(t,inv,bots,"geode")))
 #        print("bpt")
         
     if inv[ore]>=bp[obsidian][0] and inv[clay]>=bp[obsidian][1]:
         ap = (bots[0],bots[1],bots[2]+1,bots[3])
-        front.add((bp, ap, (inv[ore]-bp[obsidian][0],inv[clay]-bp[obsidian][1],inv[obsidian],inv[geode]),obsidian,t+1,tl+mkstr(t,inv,bots,"obs")))
+        front.add((bp, ap, (inv[ore]-bp[obsidian][0],inv[clay]-bp[obsidian][1],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"obs")))
         #        print("bpt2")
 
     if inv[ore]>=bp[clay]:
         ap = (bots[0],bots[1]+1,bots[2],bots[3])
-        front.add((bp, ap, (inv[ore]-bp[clay],inv[clay],inv[obsidian],inv[geode]),clay,t+1,tl+mkstr(t,inv,bots,"clay")))
+        front.add((bp, ap, (inv[ore]-bp[clay],inv[clay],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"clay")))
 #       print("bpt3")
         
     if inv[ore]>=bp[ore]:
         ap = (bots[0]+1,bots[1],bots[2],bots[3])
-        front.add((bp, ap, (inv[ore]-bp[ore],inv[clay],inv[obsidian],inv[geode]),ore,t+1,tl+mkstr(t,inv,bots,"ore")))
+        front.add((bp, ap, (inv[ore]-bp[ore],inv[clay],inv[obsidian],inv[geode]),t+1,tl+mkstr(t,inv,bots,"ore")))
 #        print("bpt4")
         
-    front.add((bp, tuple(bots), tuple(inv),False,t+1,tl+mkstr(t,inv,bots,"")))
+    front.add((bp, tuple(bots), tuple(inv),t+1,tl+mkstr(t,inv,bots,"")))
 #    print("bpt5")
                   
-front.add((bp[1],(1,0,0,0),(0,0,0,0),False,1,""))
+front.add((bp[1],(1,0,0,0),(0,0,0,0),1,""))
 
 
 while len(front):
     v = front.pop()
-    (bp,bots,inv,building,t,tl) = v
-    diggy(bp,bots,inv,building,t,tl)
-    print(best,len(front))
+    (bp,bots,inv,t,tl) = v
+    diggy(bp,bots,inv,t,tl)
+    if not len(front) % 10000:
+        print(best,len(front))
 print(best)
 
