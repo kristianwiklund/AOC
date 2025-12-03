1d3ddec8b071110d981718b4fd49d44fefb11d65
Kristian Wiklund <githubkristian@snabela.nl>
2025-12-03 11:23:47 +0100

    2025 3 a

---
+++ 2025/3/3.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append("../..")
+from utilities import *
+#import networkx as nx
+#import matplotlib.pyplot as plt
+#from copy import deepcopy
+from pprint import pprint
+#from sortedcontainers import SortedList
+#from sortedcontainers import SortedDict
+#from sortedcontainers import SortedSet
+#import numpy as np
+#import scipy
+#from functools import cache
+
+arr = readarray("input",split="",convert=lambda x:int(x))
+#lines = readlines("input.short")
+#print(arr)
+
+s=0
+for i,v in enumerate(arr):
+    #    print (i,v)
+    vv = sorted(v, reverse=True)
+    #    print(v,vv)
+
+    for t in vv:
+        try:
+            p = arr[i][:-1].index(t)
+#            print(p,arr[i][p])
+            break
+        except:
+            continue
+
+
+    for t in vv:
+        try:
+            pp = arr[i][p+1:].index(t)+p+1
+            break
+        except:
+            continue
+
+    print(i, arr[i][p], arr[i][pp],p,pp)
+    s+=arr[i][p]*10+arr[i][pp]
+
+print("part 1:",s)
+    
