9904291dd9796755020a20561bdb881f7922a980
Kristian Wiklund <githubkristian@snabela.nl>
2025-12-11 16:47:36 +0100

    x

--- 2025/10/10b.py
+++ 2025/10/10b.py
@@ -55,13 +55,99 @@ lines=[pl(x) for x in lines]
 # Here, the weight of knapsack item i is given by a D-dimensional vector (the buttons)
 # and the knapsack has a D-dimensional capacity vector
 
-bl = lines[0][1]
-targ = lines[0][2]
 
-#print(bl)
+def clack(vr, vp, clicks, mnum, ack):
+
+        
+    # subtract the clicks from the target state
+    vr = deepcopy(vr)
+    vr-=vp*mnum
+    clicks+=mnum
+
+    if sum(vr)==0:
+        return clicks
+
+#    print(vr)
+
+    #    print(vr, bl)
+    # no solution in this branch
+    
+    if not bl:
+        return None
+
+    kossan=None
+
+    for i in range(len(bl)):
+        if not i in ack:
+            kalven=pluck(vr, i, bl, clicks, ack+[i])
+        
+            if  kalven and (not kossan or kalven<kossan):
+                kossan=kalven
+            
+    return kossan
+
+    
 
 
-print(targ)
-pprint(bl)
+def pluck(remains, button, bl, clicks,ack=None):
+
+    vr = np.array(remains)
+    vp = np.array(bl[button])
+
+
+    # first match if we can match anything with this button
+    vd = vr*vp
+    vd = vd[vd != 0]
+
+    # no solution will happen with this button, we match nothing
+    if not len(vd):
+        return None
+
+    # check that we are not trying to subtract from zeros
+    y = sum([vr[i]>0 for i in range(len(vr)) if vp[i]>0])
+    if y<sum(vp):
+        return None
+
+    # then run a descent for all options of clicks from 1 to min(vd)
+
+    kossan=None
+    for i in range(min(vd),max(0,min(vd)-2),-1):
+#        print(ack,"clacking",i)
+        kalven = clack(vr, vp, clicks, i, ack)
+        if kalven and (not kossan or kalven<kossan):
+            kossan = kalven
+ 
+    return kossan
+
+
+s=0
+for l in lines:
+    bl = l[1]
+    targ = l[2]
+
+    #print(bl)
+    
+    
+    #    print(targ)
+    #    pprint(bl)
+    
+    m=None
+    for i in range(len(bl)):
+        print("clicking button",i)
+        n = pluck(targ, i,bl,0,[i])
+
+        if n and (not m or n<m):
+#            print(n)
+            m=n
+
+    if m:
+        s+=m
+    else:
+        print("no solution found:", l)
+
+print(s)
+assert(s<128003850)
+assert(s<167000508)
+
 
 
