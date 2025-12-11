db5fb0da51c617a27880a6cb99ed532914fc597d
Kristian Wiklund <githubkristian@snabela.nl>
2025-12-11 15:18:28 +0100

    x

--- 2025/10/10b.py
+++ 2025/10/10b.py
@@ -8,75 +8,58 @@ from pprint import pprint
 #from sortedcontainers import SortedList
 #from sortedcontainers import SortedDict
 #from sortedcontainers import SortedSet
-#import numpy as np
+import numpy as np
 #import scipy
 from functools import cache
+from functools import lru_cache
+import itertools
+
 #from shapely.geometry.polygon import Polygon
 #from shapely import contains
 
 #arr = readarray("input.short",split="",convert=lambda x:x)
 lines = readlines("input")
 
+
+def tf(button,ln):
+
+    ack=[]
+    for i in range(ln):
+        ack.append(1 if i in button else 0)
+
+    return (ack)
+
+assert(tf([0,2],4)==[1,0,1,0])
+
 def pl(l):
     x,y=l.split("]")
     y,z=y.split("{")
     #    print(x,y,z)
 
-    m = [list(x.replace("[","")),eval("("+y.lstrip().rstrip().replace(" ",",")+")"),eval("["+z.strip().replace("}","]"))]
-    #    m = [list(x.replace("[","")),eval("["+y.lstrip().rstrip().replace(" ",",").replace("(","[").replace(")","]")+"]"),eval("["+z.strip().replace("}","]"))]
+    #m = [list(x.replace("[","")),eval("("+y.lstrip().rstrip().replace(" ",",")+")"),eval("["+z.strip().replace("}","]"))]
+    m = [list(x.replace("[","")),eval("["+y.lstrip().rstrip().replace(" ",",").replace("(","[").replace(")","]")+"]"),eval("["+z.strip().replace("}","]"))]
+
+    m[1] = sorted([tf(x,len(m[2])) for x in m[1]],key=lambda x:-sum(x))
+    
     return m
+
 
 lines=[pl(x) for x in lines]
 
-@cache
-def clickzor(jolts, button, target, buttons, depth=0,  vom=100):
+# pl[2] contains the target
+# pl[1] contains the button list
 
-    if depth>=vom:
-        return False
+#print(lines[0])
 
-    jolts = list(jolts)
-    ac=[]
-    for x in range(len(jolts)):
-        if isinstance(buttons[button],int):
-            b=[buttons[button]]
-        else:
-            b=buttons[button]
+# idea: now combine the button presses in a way that integer-divides cleanly with the target
+# meaning that we need to start by factorizing the target
 
-#        print(b)
-        if x in list(b):
-#            print(x,"-",jolts,"-",b,jolts[x])
-            y = jolts[x]+1
-            ac.append(y)
-            if ac[-1]>target[x]:
-                return False
-        else:
- #           print(x)
-            ac.append(jolts[x])
-            
-    jolts=tuple(ac)
-    if jolts==target:
-        return depth
-    
-    for i in range(len(buttons)):
-        if i==button:
-            continue
+bl = lines[0][1]
+targ = lines[0][2]
 
-        res = clickzor(jolts, i, target, buttons, depth+1, vom)
-        if res and res<vom:
-            vom=res
+#print(bl)
 
- #   print("JT",jolts,target)
-    return vom
-
-
-# run through the problems, flip them off...
-s=0
-for m in lines:
-    print("---",m,len(m[0]))
-    v = tuple([0]*len(m[0]))
-#    print(v)
-    r = sorted(map(lambda x:clickzor(v, x, tuple(m[2]),tuple(m[1]), depth=1), range(len(m[1]))))
-    s+=r[0]
-#    print(r)
-print("part 1:",s)
-
+tf = [sfactors(x) for x in targ]
+print(targ)
+pprint(tf)
+pprint(bl)
